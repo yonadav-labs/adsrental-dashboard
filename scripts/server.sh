764eeb6b3e0ed7d1448b818771cbdd88f98571ef
Vlad Emelianov <volshebnyi@gmail.com>
2017-11-03 16:28:15 -0400

    Dev server listens on port 8007

--- scripts/server.sh
+++ scripts/server.sh
@@ -7,7 +7,7 @@ source venv/bin/activate
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:8006 \
+    --bind 0.0.0.0:8007 \
     --workers 3
    --timeout 300
 
