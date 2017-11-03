267c17dd686038e93355e500297d968a8a933cc2
Vlad Emelianov <volshebnyi@gmail.com>
2017-11-03 17:15:55 -0400

    Migrate DB on update

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,6 +4,17 @@ cd $ROOT_PATH
 
 source venv/bin/activate
 
+#waiting for db
+./scripts/wait-for-it.sh db:3306
+
+#create database if it is not there yet
+python -c 'import MySQLdb; MySQLdb.connect(user="root", host="db").cursor().execute("create database if not exists adsrental;")'
+
+#install DB and fixtures
+python manage.py migrate adsrental
+python manage.py migrate
+python manage.py loaddata adsrental/fixtures/fixtures.json
+
 # Start Gunicorn processes
 echo Starting Gunicorn.
 exec gunicorn config.wsgi:application \
