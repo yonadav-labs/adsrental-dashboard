a30120b82c2946b1fcd071530421e57e56606800
Vlad Emelianov <volshebnyi@gmail.com>
2017-12-05 14:14:21 -0400

    Add admin filters

--- adsrental/admin.py
+++ adsrental/admin.py
@@ -1,8 +1,82 @@
+import datetime
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.humanize.templatetags.humanize import naturaltime
+from django.utils import timezone
+from django.contrib.admin import SimpleListFilter
 
 from adsrental.models.user import User
 from adsrental.models.legacy import Lead, RaspberryPi
+
+
+class OnlineListFilter(SimpleListFilter):
+    title = 'EC2 online state'
+    parameter_name = 'online'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('online', 'Online only'),
+            ('offline', 'Offline only'),
+            ('offline_2days', 'Offline for last 2 days'),
+            ('offline_5days', 'Offline for last 5 days'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'online':
+            return queryset.filter(raspberry_pi__last_seen__gt=timezone.now() - datetime.timedelta(hours=7))
+        if self.value() == 'offline':
+            return queryset.filter(raspberry_pi__last_seen__lte=timezone.now() - datetime.timedelta(hours=7))
+        if self.value() == 'offline_2days':
+            return queryset.filter(raspberry_pi__last_seen__lte=timezone.now() - datetime.timedelta(hours=7 + 2 * 24))
+        if self.value() == 'offline_5days':
+            return queryset.filter(raspberry_pi__last_seen__lte=timezone.now() - datetime.timedelta(hours=7 + 5 * 24))
+
+
+class TunnelOnlineListFilter(SimpleListFilter):
+    title = 'Tunnel online state'
+    parameter_name = 'tunnel_online'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('online', 'Online only'),
+            ('offline', 'Offline only'),
+            ('offline_2days', 'Offline for last 2 days'),
+            ('offline_5days', 'Offline for last 5 days'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'online':
+            return queryset.filter(raspberry_pi__tunnel_last_tested__gt=timezone.now() - datetime.timedelta(hours=7))
+        if self.value() == 'offline':
+            return queryset.filter(raspberry_pi__tunnel_last_tested__lte=timezone.now() - datetime.timedelta(hours=7))
+        if self.value() == 'offline_2days':
+            return queryset.filter(raspberry_pi__tunnel_last_tested__lte=timezone.now() - datetime.timedelta(hours=7 + 2 * 24))
+        if self.value() == 'offline_5days':
+            return queryset.filter(raspberry_pi__tunnel_last_tested__lte=timezone.now() - datetime.timedelta(hours=7 + 5 * 24))
+
+
+class WrongPasswordListFilter(SimpleListFilter):
+    title = 'Wrong Password'
+    parameter_name = 'wrong_password'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('no', 'No'),
+            ('yes', 'Yes'),
+            ('yes_2days', 'Yes for 2 days'),
+            ('yes_5days', 'Yes for 5 days'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'no':
+            return queryset.filter(wrong_password=False)
+        if self.value() == 'yes':
+            return queryset.filter(wrong_password=True)
+        if self.value() == 'yes_2days':
+            return queryset.filter(wrong_password=True, raspberry_pi__last_seen__lte=timezone.now() - datetime.timedelta(hours=7 + 2 * 24))
+        if self.value() == 'yes_5days':
+            return queryset.filter(wrong_password=True, raspberry_pi__last_seen__lte=timezone.now() - datetime.timedelta(hours=7 + 5 * 24))
 
 
 class CustomUserAdmin(UserAdmin):
@@ -21,12 +95,34 @@ class CustomUserAdmin(UserAdmin):
 
 class LeadAdmin(admin.ModelAdmin):
     model = Lead
-    list_display = ('leadid', 'first_name', 'last_name', 'email', 'phone', 'google_account', 'facebook_account', 'raspberry_pi', )
+    list_display = ('leadid', 'name', 'email', 'phone', 'google_account', 'facebook_account', 'raspberry_pi', 'first_seen', 'last_seen', 'online', 'tunnel_online', 'wrong_password')
+    list_filter = (OnlineListFilter, TunnelOnlineListFilter, WrongPasswordListFilter, )
+    select_related = ('raspberry_pi', )
+    search_fields = ('leadid', 'first_name', 'last_name', 'raspberry_pi__rpid', )
+
+    def name(self, obj):
+        return '{} {}'.format(obj.first_name, obj.last_name)
+
+    def online(self, obj):
+        return obj.raspberry_pi.online()
+
+    def tunnel_online(self, obj):
+        return obj.raspberry_pi.tunnel_online()
+
+    def first_seen(self, obj):
+        return naturaltime(obj.raspberry_pi.first_seen)
+
+    def last_seen(self, obj):
+        return naturaltime(obj.raspberry_pi.last_seen)
+
+    online.boolean = True
+    tunnel_online.boolean = True
 
 
 class RaspberryPiAdmin(admin.ModelAdmin):
     model = RaspberryPi
     list_display = ('rpid', 'leadid', 'ipaddress', 'ec2_hostname', 'first_seen', 'last_seen', 'tunnel_last_tested', )
+    search_fields = ('leadid', 'rpid', 'ec2_hostname', )
 
 
 admin.site.register(User, CustomUserAdmin)
--- adsrental/models/legacy.py
+++ adsrental/models/legacy.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from django.utils import timezone
 from django.db import models
 
 
@@ -13,12 +14,15 @@ class Lead(models.Model):
     account_name = models.CharField(max_length=255)
     usps_tracking_code = models.CharField(max_length=255, blank=True, null=True)
     utm_source = models.CharField(max_length=80, blank=True, null=True)
-    google_account = models.IntegerField()
-    facebook_account = models.IntegerField()
+    google_account = models.BooleanField(default=False)
+    facebook_account = models.BooleanField(default=False)
     raspberry_pi = models.ForeignKey('adsrental.RaspberryPi', null=True, blank=True)
     wrong_password = models.BooleanField(default=False)
     bundler_paid = models.BooleanField(default=False)
     pi_delivered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.leadid
 
     class Meta:
         db_table = 'lead'
@@ -32,6 +36,21 @@ class RaspberryPi(models.Model):
     first_seen = models.DateTimeField()
     last_seen = models.DateTimeField()
     tunnel_last_tested = models.DateTimeField(blank=True, null=True)
+
+    def online(self):
+        if self.last_seen is None:
+            return False
+
+        return (timezone.now() - self.last_seen).total_seconds() < 7 * 60 * 60
+
+    def tunnel_online(self):
+        if self.tunnel_last_tested is None:
+            return False
+
+        return (timezone.now() - self.tunnel_last_tested).total_seconds() < 7 * 60 * 60
+
+    def __str__(self):
+        return self.rpid
 
     class Meta:
         db_table = 'raspberry_pi'
