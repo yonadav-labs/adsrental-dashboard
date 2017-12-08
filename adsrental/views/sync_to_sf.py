bc63ef27091b6911a9e87d4dcf2b341157d0aff5
Vlad Emelianov <volshebnyi@gmail.com>
2017-12-08 09:59:30 -0400

    Update sync to SF

--- adsrental/views/sync_to_sf.py
+++ adsrental/views/sync_to_sf.py
@@ -11,11 +11,15 @@ class SyncToSFView(View):
     def get(self, request):
         seconds_ago = int(request.GET.get('seconds_ago', '300'))
         raspberry_pis = RaspberryPi.objects.filter(updated__gte=timezone.now() - datetime.timedelta(seconds=seconds_ago))
-        RaspberryPi.upsert_to_sf(raspberry_pis)
+        if not request.GET.get('test'):
+            RaspberryPi.upsert_to_sf(raspberry_pis)
 
         leads = Lead.objects.filter(updated__gte=timezone.now() - datetime.timedelta(seconds=seconds_ago))
-        Lead.upsert_to_sf(leads)
+        if not request.GET.get('test'):
+            Lead.upsert_to_sf(leads)
 
         return JsonResponse({
             'result': True,
+            'raspberry_pi_rpids': [i.rpid for i in raspberry_pis],
+            'leads_ids': [i.leadid for i in leads],
         })
