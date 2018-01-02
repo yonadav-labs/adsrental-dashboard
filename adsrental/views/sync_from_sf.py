99cbb21a73b3044486dbc3e9e9a44d49f0bca6ed
Vlad Emelianov <volshebnyi@gmail.com>
2018-01-01 23:35:39 -0400

    Filter by lead ids

--- adsrental/views/sync_from_sf.py
+++ adsrental/views/sync_from_sf.py
@@ -13,7 +13,8 @@ class SyncFromSFView(View):
         minutes_ago = int(request.GET.get('minutes')) if request.GET.get('minutes') else 15
         last_touch_date_min = timezone.now() - datetime.timedelta(minutes=minutes_ago)
         sf_leads = SFLead.objects.filter(last_touch_date__gt=last_touch_date_min).simple_select_related('raspberry_pi')
-        leads = Lead.objects.all().select_related('raspberry_pi')
+        sf_leads_ids = [i.id for i in sf_leads]
+        leads = Lead.objects.filter(leadid__in=sf_leads_ids).select_related('raspberry_pi')
 
         leads_map = {}
         for lead in leads:
@@ -24,5 +25,7 @@ class SyncFromSFView(View):
 
         return JsonResponse({
             'result': True,
+            'sf_leads_ids': sf_leads_ids,
             'sf_leads': len(sf_leads),
+            'leads': len(leads),
         })
