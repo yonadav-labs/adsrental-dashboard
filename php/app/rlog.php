ae7f5108e520b112300c99ceac9b21b853d1a618
Vlad Emelianov <volshebnyi@gmail.com>
2018-01-05 14:07:21 -0400

    Remove useless message

--- php/app/rlog.php
+++ php/app/rlog.php
@@ -127,10 +127,10 @@
 		// PING
 		else if (array_key_exists("p", $_GET))
 		{
-			logEntry($_GET["rpid"], "PING");
+			// logEntry($_GET["rpid"], "PING");
 			// $updateFromSF = (date("i", time()) % 30 == 0) || isset($_GET['update']);
 			$updateFromSF = false;
-			echo nowSFFormat() . " from ". $_SERVER["REMOTE_ADDR"];
+			echo "PING at " . nowSFFormat() . " from ". $_SERVER["REMOTE_ADDR"];
 			if ($_SERVER["REMOTE_ADDR"] == "24.234.215.233") {
 				createOrUpdateSFObject("Raspberry_Pi__c", "Name", $_GET["rpid"], array("Tested_Internal__c" => true));
 			}
@@ -172,7 +172,7 @@ EOD
 				);
 				echo "State updated\n";
 			}
-			echo "Pong\n";
+			// echo "Pong\n";
 		}
 	}
 
