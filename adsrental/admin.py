b1bdbaab68e55dc92617f05541c04b896f38e413
Vlad Emelianov <volshebnyi@gmail.com>
2018-01-16 01:19:51 -0400

    Add event admin

--- adsrental/admin.py
+++ adsrental/admin.py
@@ -372,16 +372,16 @@ class RaspberryPiAdmin(admin.ModelAdmin):
 
 class CustomerIOEventAdmin(admin.ModelAdmin):
     model = CustomerIOEvent
-    # list_display = ('id', 'lead', 'lead_name', 'lead_email', 'name', 'created')
-    # search_fields = ('lead__email', 'lead__first_name', 'lead__last_name', 'name', )
-    # list_filter = ('name', )
-    # readonly_fields = ('created', )
+    list_display = ('id', 'lead', 'lead_name', 'lead_email', 'name', 'created')
+    search_fields = ('lead__email', 'lead__first_name', 'lead__last_name', 'name', )
+    list_filter = ('name', )
+    readonly_fields = ('created', )
 
-    # def lead_email(self, obj):
-    #     return obj.lead.email
+    def lead_email(self, obj):
+        return obj.lead.email
 
-    # def lead_name(self, obj):
-    #     return obj.lead.name()
+    def lead_name(self, obj):
+        return obj.lead.name()
 
 
 admin.site.register(User, CustomUserAdmin)
