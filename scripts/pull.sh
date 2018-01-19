d9059e0ddb836d33ac632430f7f28e4c3dce9c1d
Vlad Emelianov <volshebnyi@gmail.com>
2018-01-18 23:39:25 -0400

    Update pull scripts

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
 cd ~/dashboard/
 git pull
 
-docker-compose build
+docker-compose -f docker-compose.dev.yml build
+
+if [ "$1" = "migrate" ]; then
+    echo "migrate"
+    docker-compose -f docker-compose.dev.yml run web python manage.py migrate adsrental
+fi
 
 #docker-compose -f docker-compose.dev.yml run web python manage.py migrate
 #docker-compose -f docker-compose.dev.yml run web python manage.py loaddata fixtures
