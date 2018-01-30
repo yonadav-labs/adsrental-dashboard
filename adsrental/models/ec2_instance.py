6674a1a818e2e6b174349912d41724a0903afed9
Vlad Emelianov <volshebnyi@gmail.com>
2018-01-29 20:32:37 -0400

    Decrease timeouts

--- adsrental/models/ec2_instance.py
+++ adsrental/models/ec2_instance.py
@@ -144,7 +144,7 @@ class EC2Instance(models.Model):
         private_key = paramiko.RSAKey.from_private_key_file(settings.FARMBOT_KEY)
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(self.ip_address, username='Administrator', port=40594, pkey=private_key, timeout=20)
+        ssh.connect(self.ip_address, username='Administrator', port=40594, pkey=private_key, timeout=5)
         return ssh
 
     def ssh_execute(self, cmd, input=None, ssh=None):
@@ -277,7 +277,7 @@ class EC2Instance(models.Model):
     def troubleshoot_web(self):
         response = None
         try:
-            response = requests.get('http://{}:13608'.format(self.hostname), timeout=10)
+            response = requests.get('http://{}:13608'.format(self.hostname), timeout=5)
         except Exception:
             self.web_up = False
             return
