750da5748110855c7a02c43da5927018dff09e44
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-05 23:11:50 -0400

    Use 13 workers

--- scripts/server.sh
+++ scripts/server.sh
@@ -20,7 +20,7 @@ echo Starting Gunicorn.
 exec gunicorn config.wsgi:application \
     --worker-class gevent \
     --bind 0.0.0.0:8007 \
-    --workers 8 \
+    --workers 13 \
     --timeout 3000 \
     --graceful-timeout 3000 \
     --worker-connections 10000 \
