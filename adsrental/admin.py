fbcc0875e44452eb4a19c76d323f02dd5d8067a0
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-07 13:46:00 -0400

    Add export to CSV

--- adsrental/admin.py
+++ adsrental/admin.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 
+import csv
 import datetime
 from dateutil.relativedelta import relativedelta
 
+from django.http import HttpResponse
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth.admin import UserAdmin
@@ -918,9 +920,27 @@ class LeadHistoryMonthAdmin(admin.ModelAdmin):
         'days_wrong_password',
         'amount',
     )
+    csv_fields = (
+        ('leadid', 'Lead'),
+        ('rpid', 'RPID', ),
+        ('lead__first_name', 'First Name', ),
+        ('lead__last_name', 'Last Name', ),
+        ('lead__street', 'Street', ),
+        ('lead__city', 'City', ),
+        ('lead__state', 'State', ),
+        ('lead__postal_code', 'Postal Code', ),
+        ('days_online', 'Days online'),
+        ('days_offline', 'Days offline'),
+        ('days_wrong_password', 'Days wrong password'),
+        ('amount', 'Amount'),
+    )
     search_fields = ('lead__raspberry_pi__rpid', 'lead__first_name', 'lead__last_name', 'lead__email', 'lead__phone', )
     list_filter = (DateMonthListFilter, HistoryStatusListFilter, )
     list_select_related = ('lead', 'lead__raspberry_pi')
+    actions = ('export_as_csv', )
+
+    def leadid(self, obj):
+        return obj.lead and obj.lead.sf_leadid
 
     def rpid(self, obj):
         return obj.lead and obj.lead.raspberry_pi and obj.lead.raspberry_pi.rpid
@@ -945,6 +965,32 @@ class LeadHistoryMonthAdmin(admin.ModelAdmin):
             lead=lead.name(),
             q=lead.leadid,
         )
+
+    def export_as_csv(self, request, queryset):
+        field_names = [i[0] for i in self.csv_fields]
+        field_titles = [i[1] for i in self.csv_fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename=check_report_{}.csv'.format(request.GET.get('date', ''))
+
+        writer = csv.writer(response)
+        writer.writerow(field_titles)
+        for obj in queryset:
+            row = []
+            for field in field_names:
+                if hasattr(self, field) and callable(getattr(self, field)):
+                    row.append(getattr(self, field)(obj))
+                    continue
+                if hasattr(obj, field) and callable(getattr(obj, field)):
+                    row.append(getattr(obj, field)())
+                    continue
+
+                item = obj
+                for subfield in field.split('__'):
+                    item = getattr(item, subfield)
+                row.append(item)
+            writer.writerow(row)
+        return response
 
     lead_link.short_description = 'Lead'
     lead_link.allow_tags = True
