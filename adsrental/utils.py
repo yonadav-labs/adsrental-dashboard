a79c161932b6a0ac6e4b3f26e53f0476c6ed6ac8
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-08 17:37:51 -0400

    Fix typo

--- adsrental/utils.py
+++ adsrental/utils.py
@@ -75,7 +75,7 @@ class ShipStationClient(object):
     def add_lead_order(self, lead):
         if not lead.shipstation_order_number:
             random_str = str(uuid.uuid4()).replace('-', '')[:10]
-            lead.shipstation_order_number = '{}__{}' % (lead.raspberry_pi.rpid, random_str)
+            lead.shipstation_order_number = '{}__{}'.format(lead.raspberry_pi.rpid, random_str)
 
         order = ShipStationOrder(
             order_key=lead.shipstation_order_number, order_number=lead.shipstation_order_number)
