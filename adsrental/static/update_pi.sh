cff51b970bb40ee20e48398c8d197a7e8650525f
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-11 01:50:58 -0400

    RPi 1.0.17

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -3,6 +3,6 @@
 cd /home/pi/new-pi/
 
 sudo apt install -y ssh
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.16.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.17.zip > pi_patch.zip
 unzip -o pi_patch.zip
 sudo reboot
