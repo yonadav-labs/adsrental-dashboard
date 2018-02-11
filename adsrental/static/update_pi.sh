43dc180f49a5e0faf9de4238d88226231a0e7be8
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-11 06:47:38 -0400

    RPi 1.0.20

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -2,8 +2,8 @@
 
 cd /home/pi/new-pi/
 
-sudo apt install -y ssh
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.19.zip > pi_patch.zip
+# sudo apt install -y ssh
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.20.zip > pi_patch.zip
 unzip -o pi_patch.zip
 
 cat /home/pi/new-pi/crontab.txt | crontab
