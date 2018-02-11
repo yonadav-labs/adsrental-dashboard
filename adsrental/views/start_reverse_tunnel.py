b653c3da468f9c90505e98edb18e49d0f7f2a1f7
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-11 06:11:56 -0400

    Fix typo

--- adsrental/views/start_reverse_tunnel.py
+++ adsrental/views/start_reverse_tunnel.py
@@ -8,6 +8,6 @@ from adsrental.models.ec2_instance import EC2Instance
 
 class StartReverseTunnelView(View):
     def get(self, request, rpid):
-        ec2_instance = EC2Instance.obejcts.get(rpid=rpid)
+        ec2_instance = EC2Instance.objects.get(rpid=rpid)
         ec2_instance.ssh_execute('ssh -N -D 3808 -p 2046 pi@localhost')
         return JsonResponse(dict(result=True))
