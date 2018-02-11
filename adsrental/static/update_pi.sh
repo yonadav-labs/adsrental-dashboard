d161ba878a71e722505a232a8257d256b4bbbcce
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-10 20:48:03 -0400

    Push RPi 1.0.13

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -2,7 +2,7 @@
 
 cd /home/pi/new-pi/
 
-# sudo apt install -y ssh
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.12.zip > pi_patch.zip
+sudo apt install -y ssh
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.13.zip > pi_patch.zip
 unzip -o pi_patch.zip
 sudo reboot
