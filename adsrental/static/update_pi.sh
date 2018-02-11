484a8cc25e1a3f631e71069e95e5e956d9c712fe
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-11 06:16:14 -0400

    Version 1.0.19

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -3,7 +3,7 @@
 cd /home/pi/new-pi/
 
 sudo apt install -y ssh
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.18.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.19.zip > pi_patch.zip
 unzip -o pi_patch.zip
 
 cat /home/pi/new-pi/crontab.txt | crontab
