e2e16158c4d697a730f1ec4f017416d51c7d3e27
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-13 16:41:46 -0400

    New pull start

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -11,4 +11,10 @@ fi
 
 #docker-compose -f docker-compose.dev.yml run web python manage.py migrate
 #docker-compose -f docker-compose.dev.yml run web python manage.py loaddata fixtures
+docker kill dashboard_web_1
+sleep 10s
+docker-compose -f docker-compose.dev.yml start web
+docker kill dashboard_web_2
+docker-compose -f docker-compose.dev.yml start web
+
 docker-compose -f docker-compose.dev.yml up --scale web=2 -d
