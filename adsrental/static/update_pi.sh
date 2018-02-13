2c2605709fc87b5ce8da6477ae4e4122c566a3fd
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-13 14:50:53 -0400

    Update pi changes

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -2,9 +2,12 @@
 
 cd /home/pi/new-pi/
 
-# sudo apt install -y ssh
+# sudo apt install -y ssh libffi-dev python-pip openssl libssl-dev
+# sudo pip install -U pip
+# sudo pip install paramiko requests
 curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.20.zip > pi_patch.zip
 unzip -o pi_patch.zip
 
+# ./install.sh
 cat /home/pi/new-pi/crontab.txt | crontab
 sudo reboot
