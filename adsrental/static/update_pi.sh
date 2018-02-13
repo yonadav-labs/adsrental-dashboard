825ec2926bc7a812462a6366976ee0e71fba31f2
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-13 17:53:29 -0400

    Update Pis to 1.0.21

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -5,7 +5,7 @@ cd /home/pi/new-pi/
 # sudo apt install -y ssh libffi-dev python-pip openssl libssl-dev
 # sudo pip install -U pip
 # sudo pip install paramiko requests
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.20.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.21.zip > pi_patch.zip
 unzip -o pi_patch.zip
 
 # ./install.sh
