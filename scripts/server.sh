e118f564c5fa64d9c038adfdc4904e8855c6094e
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-20 16:54:24 -0400

    Use reload on prod

--- scripts/server.sh
+++ scripts/server.sh
@@ -17,7 +17,7 @@ python manage.py collectstatic --noinput > /dev/nul
 
 # Start Gunicorn processes
 echo Starting Gunicorn.
-gunicorn -D config.wsgi:application -b 0.0.0.0:80
+gunicorn -D config.wsgi:application -b 0.0.0.0:80 --reload
 gunicorn config.wsgi:application \
   --bind 0.0.0.0:443 \
   --certfile=/app/cert/adsrental_com.crt \
@@ -29,4 +29,5 @@ gunicorn config.wsgi:application \
   --graceful-timeout 3000 \
   --worker-connections 10000 \
   --max-requests 10000 \
-  --error-logfile=-
+  --error-logfile=- \
+  --reload
