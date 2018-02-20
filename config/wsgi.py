c561e78741527c2c28e1ccca6610794bc391b6b8
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-20 16:01:37 -0400

    Debug using werkzeug

--- config/wsgi.py
+++ config/wsgi.py
@@ -2,9 +2,11 @@ import os
 
 import pymysql
 from django.core.wsgi import get_wsgi_application
+from werkzeug.debug import DebuggedApplication
 
 from config.environment import SETTINGS_MODULE
 
 pymysql.install_as_MySQLdb()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", SETTINGS_MODULE)
 application = get_wsgi_application()
+application = DebuggedApplication(application, evalex=True)
--- scripts/runserver.sh
+++ scripts/runserver.sh
@@ -8,6 +8,8 @@ cd $ROOT_PATH
 #create database if it is not there yet
 #python -c 'import pymysql; pymysql.connect(user="root", host="db").cursor().execute("create database if not exists adsrental;")'
 
+python manage.py collectstatic --noinput > /dev/nul
+
 #install DB and fixtures
 # python manage.py makemigrations adsrental
 # python manage.py migrate adsrental
