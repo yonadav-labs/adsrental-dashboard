4f230f718e616a9f89ebbb3c5ad473a496fde775
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-20 20:14:46 -0400

    Update pull

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,12 +2,14 @@
 cd ~/dashboard/
 git pull
 
-docker-compose -f docker-compose.dev.yml build
 
 if [ "$1" = "restart" ]; then
     echo "restart"
+    docker-compose -f docker-compose.dev.yml build
     docker-compose -f docker-compose.dev.yml run web python manage.py migrate adsrental
     docker-compose -f docker-compose.dev.yml up --remove-orphans -d
+else
+    docker exec -ti dashboard_web_1 python manage.py collectstatic --noinput > /dev/null
 fi
 
 #docker-compose -f docker-compose.dev.yml run web python manage.py migrate
