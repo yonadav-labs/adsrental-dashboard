87487a8a99a61c4f37062e8be59cf118861c8a9d
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-21 15:40:08 -0400

    Update_pi to 1.0.25

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -5,9 +5,9 @@ cd /home/pi/new-pi/
 # sudo apt install -y ssh libffi-dev python-pip openssl libssl-dev
 # sudo pip install -U pip
 # sudo pip install paramiko requests
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.24.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.25.zip > pi_patch.zip
 unzip -o pi_patch.zip
 
 # ./install.sh
 cat /home/pi/new-pi/crontab.txt | crontab
-sudo reboot
+# sudo reboot
