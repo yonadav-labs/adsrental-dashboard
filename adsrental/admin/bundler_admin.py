db7fd1a20f282004f659fee84139b093e178e6ad
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-22 23:22:49 -0400

    Add leads count

--- adsrental/admin/bundler_admin.py
+++ adsrental/admin/bundler_admin.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from django.contrib import messages
+from django.db.models import Count
 
 from adsrental.models.bundler import Bundler
 from adsrental.models.lead import Lead
@@ -9,10 +10,14 @@ from adsrental.models.lead import Lead
 
 class BundlerAdmin(admin.ModelAdmin):
     model = Bundler
-    list_display = ('id', 'name', 'utm_source', 'adsdb_id', 'email', 'phone', )
+    list_display = ('id', 'name', 'utm_source', 'adsdb_id', 'email', 'phone', 'leads_count', )
     actions = (
         'assign_leads_for_this_bundler',
     )
+
+    def get_queryset(self, request):
+        qs = super(BundlerAdmin, self).get_queryset(request)
+        return qs.annotate(leads_count=Count('lead'))
 
     def assign_leads_for_this_bundler(self, request, queryset):
         for bundler in queryset:
@@ -32,3 +37,6 @@ class BundlerAdmin(admin.ModelAdmin):
                 messages.success(request, 'Bundler {} is unassigned from {} leads.'.format(
                     bundler, leads.count(),
                 ))
+
+    def leads_count(self, inst):
+        return inst.leads_count
