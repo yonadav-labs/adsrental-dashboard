752af4224fa952fc2a9ac8cf6ba16a965438f603
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-23 15:23:52 -0400

    RPi firmware 1.1.1

--- adsrental/static/restart_pi.sh
+++ adsrental/static/restart_pi.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo reboot
+# sudo reboot
--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -2,13 +2,7 @@
 
 cd /home/pi/new-pi/
 
-# sudo apt install -y ssh libffi-dev python-pip openssl libssl-dev
-# sudo pip install -U pip
-# sudo pip install paramiko requests
-# sudo apt-get install -y unzip
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.0.25.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.1.1.zip > pi_patch.zip
 unzip -o pi_patch.zip
-
-# ./install.sh
 cat /home/pi/new-pi/crontab.txt | crontab
 # sudo reboot
