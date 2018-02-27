c4099ec8f21d7bfef4a8a3f1a2ea78b661993e0a
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-27 16:33:48 -0400

    Fix hourly script

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -2,6 +2,12 @@
 
 EC2_INSTANCE="`head -n 1 ${HOME}/hostname.conf`"
 echo $EC2_INSTANCE
+HAS_FF="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Users\\Public\\Desktop\\Firefox.lnk' | grep Firefox.lnk`"
+if ! [ "$HAS_FF" == "" ]; then
+    ${HOME}/new-pi/client_log.sh "Firefox is already installed..."
+    exit
+fi
+
 HAS_INST="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\firefox_inst.exe' | grep firefox_inst`"
 echo $HAS_INST
 
@@ -9,8 +15,10 @@ if [ "$HAS_INST" == "" ]; then
     echo "Downloading"
     ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\firefox_inst.exe https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
     echo "Done"
+    ${HOME}/new-pi/client_log.sh "Firefox is downloaded..."
 fi
 
 echo "Installing"
 ssh Administrator@${EC2_INSTANCE} -p 40594 '@start /wait "Firefox" "C:\firefox_inst.exe" -ms'
 echo "Installed"
+${HOME}/new-pi/client_log.sh "Firefox is installed"
