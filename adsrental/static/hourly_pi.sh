9e664d61ae7153ee821e326144f04119958530c6
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-27 18:25:38 -0400

    Update hourly

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 
 EC2_INSTANCE="`head -n 1 ${HOME}/hostname.conf`"
-echo $EC2_INSTANCE
-HAS_FF="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Users\\Public\\Desktop\\Firefox.lnk' | grep Firefox.lnk`"
-if ! [ "$HAS_FF" == "" ]; then
-    ${HOME}/new-pi/client_log.sh "Firefox is already installed..."
-    exit
-fi
-
-HAS_INST="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\firefox_inst.exe' | grep firefox_inst`"
-echo $HAS_INST
-
-if [ "$HAS_INST" == "" ]; then
-    echo "Downloading"
-    ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\firefox_inst.exe https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
-    echo "Done"
-    ${HOME}/new-pi/client_log.sh "Firefox is downloaded..."
-fi
-
-echo "Installing"
-ssh Administrator@${EC2_INSTANCE} -p 40594 '@start /wait "Firefox" "C:\firefox_inst.exe" -ms'
-echo "Installed"
+${HOME}/new-pi/client_log.sh "Hourly script for ${EC2_INSTANCE}"
 
 HAS_FF="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Users\\Public\\Desktop\\Firefox.lnk' | grep Firefox.lnk`"
-if ! [ "$HAS_FF" == "" ]; then
-    ${HOME}/new-pi/client_log.sh "Firefox is installed"
-    exit
+if [ "$HAS_FF" == "" ]; then
+    ${HOME}/new-pi/client_log.sh "Firefox is not installed..."
+    HAS_FF_INST="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\firefox_inst.exe' | grep firefox_inst`"
+    if [ "$HAS_FF_INST" == "" ]; then
+        ${HOME}/new-pi/client_log.sh "Firefox is not downloaded..."
+        ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\firefox_inst.exe https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+    fi
+    ssh Administrator@${EC2_INSTANCE} -p 40594 '@start /wait "Firefox" "C:\firefox_inst.exe" -ms'
+    HAS_FF="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Users\\Public\\Desktop\\Firefox.lnk' | grep Firefox.lnk`"
+    if [ "$HAS_FF" == "" ]; then
+        ${HOME}/new-pi/client_log.sh "Firefox is still missing..."
+    fi
+else
+    ${HOME}/new-pi/client_log.sh "Firefox is already installed"
 fi
-${HOME}/new-pi/client_log.sh "Firefox is still missing"
+
+HAS_ANTIDETECT="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Antidetect\\antidetect.exe' | grep antidetect.exe`"
+if [ "$HAS_ANTIDETECT" == "" ]; then
+    ${HOME}/new-pi/client_log.sh "Antidetect is not installed..."
+    HAS_ANTIDETECT_INST="`ssh Administrator@${EC2_INSTANCE} -p 40594 'dir C:\\Antidetect.zip' | grep Antidetect.zip`"
+    if [ "$HAS_ANTIDETECT_INST" == "" ]; then
+        ${HOME}/new-pi/client_log.sh "Antidetect is not downloaded..."
+        ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\Antidetect.zip https://s3-us-west-2.amazonaws.com/mvp-store/Antidetect.zip"
+    fi
+else
+    ${HOME}/new-pi/client_log.sh "Antidetect is already installed"
+fi
