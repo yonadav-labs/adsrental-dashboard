84f01dc2236b5b33a8acff6fef6e55dd0868920e
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-27 15:59:11 -0400

    Add hourly script

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
-# Revive pis here
-echo "placeholder"
+EC2_INSTANCE="`head -n 1 ${HOME}/hostname.conf`"
+HAS_INST=`ssh Administrator@${EC2_INSTANCE} -p 40594 "dir C:\\firefox_inst.exe" | grep firefox_inst`
+
+if [ "$HAS_INST" == "" ]; then
+    ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\firefox_inst.exe https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+fi
+
+ssh Administrator@${EC2_INSTANCE} -p 40594 '@start /wait "Firefox" "C:\firefox_inst.exe" -ms'
