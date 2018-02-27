0fcbe36c094e1cddc63c3241554383bb4ca62b89
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-27 16:11:21 -0400

    Add echo to hourly

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
 EC2_INSTANCE="`head -n 1 ${HOME}/hostname.conf`"
+echo $EC2_INSTANCE
 HAS_INST=`ssh Administrator@${EC2_INSTANCE} -p 40594 "dir C:\\firefox_inst.exe" | grep firefox_inst`
+echo $HAS_INST
 
 if [ "$HAS_INST" == "" ]; then
+    echo "Downloading"
     ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell iwr -outf C:\\firefox_inst.exe https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+    echo "Done"
 fi
 
+echo "Installing"
 ssh Administrator@${EC2_INSTANCE} -p 40594 '@start /wait "Firefox" "C:\firefox_inst.exe" -ms'
+echo "Installed"
