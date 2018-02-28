4a13133f882f1f64abfa936a4ec364e76b668e91
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-27 22:56:18 -0400

    RPi 1.1.5

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -2,7 +2,7 @@
 
 cd /home/pi/new-pi/
 
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.1.4.zip > pi_patch.zip
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.1.5.zip > pi_patch.zip
 unzip -o pi_patch.zip
 cat /home/pi/new-pi/crontab.txt | crontab
 sudo sync
