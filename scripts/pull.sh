e66c9c659d9a4dba76e12d285bf370079f1ba727
Vlad Emelianov <volshebnyi@gmail.com>
2018-02-28 22:53:40 -0400

    New pull script

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,15 +2,20 @@
 cd ~/dashboard/
 git pull
 
-
-if [ "$1" = "restart" ]; then
-    echo "restart"
+if [ "$1" = "migrate" ]; then
     docker-compose -f docker-compose.dev.yml build
     docker-compose -f docker-compose.dev.yml run web python manage.py migrate adsrental
     docker-compose -f docker-compose.dev.yml up --remove-orphans -d
-else
-    docker-compose -f docker-compose.dev.yml run web python manage.py collectstatic --noinput > /dev/null
+    exit
 fi
+if [ "$1" = "restart" ]; then
+    echo "restart"
+    docker-compose -f docker-compose.dev.yml build
+    docker-compose -f docker-compose.dev.yml up --remove-orphans -d
+    exit
+fi
+
+# docker-compose -f docker-compose.dev.yml run web python manage.py collectstatic --noinput > /dev/null
 
 #docker-compose -f docker-compose.dev.yml run web python manage.py migrate
 #docker-compose -f docker-compose.dev.yml run web python manage.py loaddata fixtures
