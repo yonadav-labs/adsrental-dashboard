da9ab7e485aff23b017c7288faad79461bc7f644
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-07 16:50:28 -0400

    FIx pull

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -3,6 +3,7 @@
 
 if [ "$1" = "migrate" ]; then
     cd ~/migrate/dashboard/
+    git pull
     docker-compose -f docker-compose.dev.yml build
     docker-compose -f docker-compose.dev.yml run web python manage.py migrate adsrental
     cd ~/dashboard/
