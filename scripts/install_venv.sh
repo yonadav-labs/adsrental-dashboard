1332eef35001e8b3f9576d9e09d320d92ce95a42
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-18 21:56:09 -0400

    Update venv

--- scripts/install_venv.sh
+++ scripts/install_venv.sh
@@ -7,9 +7,6 @@ if [ ! -d "venv" ]; then
     virtualenv venv -p /usr/bin/python3.6
 fi
 
-sudo apt install python-qt4
-
 source venv/bin/activate
-pip install flake8==3.4.1 autopep8==1.3.3 rdpy==1.3.2
+pip install flake8==3.5.0 autopep8==1.3.4
 pip install -U -r requirements.txt
-ln -s /usr/lib/python3.6/dist-packages/PyQt4/ venv/lib/python3.6/site-packages/
