98f6e868e9a8ed1ecd6c33f5686ce84ada19a6aa
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-20 11:55:00 -0400

    Stop EC2 instance when cannot get netstat

--- adsrental/views/ec2_ssh.py
+++ adsrental/views/ec2_ssh.py
@@ -1,14 +1,14 @@
-from __future__ import unicode_literals
-
+'Views used by RaspberryPi devices'
 from django.views import View
-from django.shortcuts import Http404
 from django.http import JsonResponse, HttpResponse
 
-from adsrental.models.ec2_instance import EC2Instance
+from adsrental.models.ec2_instance import EC2Instance, SSHConnectException
 
 
 class StartReverseTunnelView(View):
+    'Start reverse tunnel from EC2. Used as a fallback if RaspberryPi cannot created it by itself'
     def get(self, request, rpid):
+        'Start reverse tunnel from EC2. Used as a fallback if RaspberryPi cannot created it by itself'
         ec2_instance = EC2Instance.objects.filter(rpid=rpid.strip()).first()
         if not ec2_instance or not ec2_instance.is_running():
             return JsonResponse(dict(result=False))
@@ -18,10 +18,16 @@ class StartReverseTunnelView(View):
 
 
 class GetNetstatView(View):
+    'Get netstat output from EC2. Used as a fallback if RaspberryPi cannot get it by itself'
     def get(self, request, rpid):
+        'Get netstat output from EC2. Used as a fallback if RaspberryPi cannot get it by itself'
         ec2_instance = EC2Instance.objects.filter(rpid=rpid.strip()).first()
         if not ec2_instance or not ec2_instance.is_running():
             return HttpResponse('')
 
-        output = ec2_instance.ssh_execute('netstat -an')
+        try:
+            output = ec2_instance.ssh_execute('netstat -an')
+        except SSHConnectException:
+            ec2_instance.stop()
+            raise
         return HttpResponse(output, content_type='text/plain')
