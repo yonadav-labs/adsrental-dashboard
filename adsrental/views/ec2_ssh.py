faf60e91d1f43460c8e4b8284295347eb4fc097e
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-20 11:09:15 -0400

    Do not silent paramiko errors

--- adsrental/views/ec2_ssh.py
+++ adsrental/views/ec2_ssh.py
@@ -10,23 +10,18 @@ from adsrental.models.ec2_instance import EC2Instance
 class StartReverseTunnelView(View):
     def get(self, request, rpid):
         ec2_instance = EC2Instance.objects.filter(rpid=rpid.strip()).first()
-        if not ec2_instance:
-            raise Http404
-        try:
-            ec2_instance.ssh_execute('ssh -N -D 3808 -p 2046 pi@localhost')
-        except Exception:
+        if not ec2_instance or not ec2_instance.is_running():
             return JsonResponse(dict(result=False))
+
+        ec2_instance.ssh_execute('ssh -N -D 3808 -p 2046 pi@localhost')
         return JsonResponse(dict(result=True))
 
 
 class GetNetstatView(View):
     def get(self, request, rpid):
         ec2_instance = EC2Instance.objects.filter(rpid=rpid.strip()).first()
-        if not ec2_instance:
-            raise Http404
-        output = ''
-        try:
-            output = ec2_instance.ssh_execute('netstat -an')
-        except Exception:
-            pass
+        if not ec2_instance or not ec2_instance.is_running():
+            return HttpResponse('')
+
+        output = ec2_instance.ssh_execute('netstat -an')
         return HttpResponse(output, content_type='text/plain')
