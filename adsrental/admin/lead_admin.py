859f0bd1dbfd1b34b0a58d69a7437bc7f5a51774
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-26 17:05:47 -0400

    Fix import

--- adsrental/admin/lead_admin.py
+++ adsrental/admin/lead_admin.py
@@ -1,6 +1,7 @@
 
 from __future__ import unicode_literals
 
+from django.contrib import admin
 from django.shortcuts import render
 from django.urls import reverse
 from django.contrib import messages
