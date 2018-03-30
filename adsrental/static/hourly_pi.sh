f5dfc33919a62c0158168c0238042529979d031d
Vlad Emelianov <volshebnyi@gmail.com>
2018-03-30 19:29:22 -0400

    Delete firefox shortcut

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -13,9 +13,11 @@ ${HOME}/new-pi/client_log.sh "Hourly script for ${EC2_INSTANCE}"
 # ssh Administrator@${EC2_INSTANCE} -p 40594 "Taskkill /IM ruby.exe /F"
 # ssh Administrator@${EC2_INSTANCE} -p 40594 "powershell Rename-Item -Path C:\\Users\\Administrator\\Desktop\\auto -newName C:\\Users\\Administrator\\Desktop\\auto_backup"
 # ssh Administrator@${EC2_INSTANCE} -p 40594 'del "C:\Users\Administrator\Desktop\Restart Tunnel.url"'
+ssh Administrator@${EC2_INSTANCE} -p 40594 'del "C:\Users\Administrator\Desktop\Firefox.lnk"'
+ssh Administrator@${EC2_INSTANCE} -p 40594 'del "C:\Users\Public\Desktop\Firefox.lnk"'
 
 TASKLIST_OUTPUT=`ssh Administrator@${EC2_INSTANCE} -p 40594 'tasklist'`
-TASKLIST_FIREFOX=`echo "$NETSTAT_OUTPUT" | grep 'firefox.exe'`
+TASKLIST_FIREFOX=`echo "$TASKLIST_OUTPUT" | grep 'firefox.exe'`
 if ! [ "$TASKLIST_FIREFOX" == "" ]; then
     NETSTAT_OUTPUT=`ssh Administrator@${EC2_INSTANCE} -p 40594 'netstat -an'`
     RDP_NETSTAT=`echo "$NETSTAT_OUTPUT" | grep 'TCP' | grep 'ESTABLISHED' | grep ':23255'`
@@ -24,3 +26,5 @@ if ! [ "$TASKLIST_FIREFOX" == "" ]; then
         ssh Administrator@${EC2_INSTANCE} -p 40594 "taskkill /IM FirefoxPortable.exe /F"
     fi
 fi
+
+C:\Users\Public\Desktop\Firefox.lnk
