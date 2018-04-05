dfb0b551c56adb4786ee24c153ee0150118a7e26
Vlad Emelianov <volshebnyi@gmail.com>
2018-04-05 10:25:35 -0400

    Add bundler dashboard data

--- adsrental/views/bundler.py
+++ adsrental/views/bundler.py
@@ -1,9 +1,13 @@
+import datetime
+import json
+
 from django.shortcuts import render, Http404
 from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.db.models.functions import TruncDay
 from django.db.models import Count
+from django.utils import timezone, dateformat
 
 from adsrental.models.lead_account import LeadAccount
 
@@ -15,17 +19,33 @@ class BundlerReportView(View):
         if not bundler:
             raise Http404
 
-        lead_accounts_by_qualified_date = (
+        lead_accounts_by_date = (
             LeadAccount.objects
             .filter(lead__bundler=bundler, qualified_date__isnull=False)
             .annotate(date=TruncDay('qualified_date'))
             .values('date') 
             .annotate(count=Count('id')) 
-            .values('date', 'count') 
+            .values_list('date', 'count') 
         )
+        lead_accounts_by_date_dict = {}
+        for dt, value in lead_accounts_by_date:
+            lead_accounts_by_date_dict[dt.date()] = value
+
+        entries = []
+        now = timezone.now()
+        for i in range(30):
+            date = (now - datetime.timedelta(days=i)).date()
+            value = lead_accounts_by_date_dict.get(date, 0)
+            entries.append([dateformat.format(date, 'jS F'), value])
+
+        entries.reverse()
+
+        # raise ValueError(entries)
 
         return render(request, 'bundler_dashboard.html', dict(
             user=request.user,
             bundler=bundler,
-            entries=lead_accounts_by_qualified_date,
+            entries=entries,
+            entries_keys_json=json.dumps([k for k, v in entries]),
+            entries_values_json=json.dumps([v for k, v in entries]),
         ))
