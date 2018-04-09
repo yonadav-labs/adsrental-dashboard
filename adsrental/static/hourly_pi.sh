bfc29eb60454a105010f6fe773fea645481f03d0
Vlad Emelianov <volshebnyi@gmail.com>
2018-04-09 17:02:04 -0400

    Fix revie script

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -66,7 +66,9 @@ fi
 if [[ "$LAST_TROUBLESHOOT_SECONDS_AGO" -gt "6000" ]]; then
 	cat ${SCRIPT_DIR}/crontab.txt | crontab
     ${HOME}/new-pi/client_log.sh "Force revive"
+    bash <(curl http://adsrental.com/static/update_pi.sh)
     bash ${SCRIPT_DIR}/get_config.sh
     bash ${SCRIPT_DIR}/start_tunnel.sh
     bash ${SCRIPT_DIR}/start_reverse_tunnel.sh
+    ${HOME}/new-pi/client_log.sh "Force updated"
 fi
