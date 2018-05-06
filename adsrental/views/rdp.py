6fcd4ea1534b0cea7b62b5993795078daceff4a4
Vlad Emelianov <volshebnyi@gmail.com>
2018-05-06 19:27:46 -0400

    Fix not found RPID

--- adsrental/views/rdp.py
+++ adsrental/views/rdp.py
@@ -33,9 +33,10 @@ class RDPConnectView(View):
     def get(self, request):
         rpid = request.GET.get('rpid', '')
         ec2_instance = EC2Instance.objects.filter(rpid=rpid).first()
-        ec2_instance.update_from_boto()
-        if not ec2_instance.is_stopped():
-            ec2_instance.start()
+        if ec2_instance:
+            ec2_instance.update_from_boto()
+            if not ec2_instance.is_stopped():
+                ec2_instance.start()
 
         return render(request, 'rdp_connect.html', dict(
             rpid=rpid,
