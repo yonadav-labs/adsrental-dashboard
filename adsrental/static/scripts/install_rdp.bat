3101c7ec0fcaad54879f0bf5a8d2b66f0b434df5
Vlad Emelianov <volshebnyi@gmail.com>
2018-05-14 14:52:22 -0400

    Fix again

--- adsrental/static/scripts/install_rdp.bat
+++ adsrental/static/scripts/install_rdp.bat
@@ -1,38 +1,11 @@
 
-Save New Duplicate & Edit Just Text Twitter
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
 @echo off
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 
 if "%~1"=="-d" (
 cls
-del %HOMEPATH%\hyperlink-rdp.ps1
+del "%HOMEPATH%\hyperlink-rdp.ps1"
 reg delete "HKCR\rdp" /f
 echo RDP:// HyperLink uninstalled successfully
 pause
