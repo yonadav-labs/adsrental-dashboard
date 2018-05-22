22377994db77607e0da4a263cc7209273347a3a8
Vlad Emelianov <volshebnyi@gmail.com>
2018-05-21 23:50:59 -0400

    Updat eantidetect with no config overwrite

--- adsrental/static/antidetect/install_antidetect.bat
+++ adsrental/static/antidetect/install_antidetect.bat
@@ -3,23 +3,18 @@
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 
 if exist C:\Antidetect_7.3.1.zip (
-    echo "[!] Latest Antidetect is already downloaded. TO download again, please delete C:\Antidetect_7.3.1.zip file"
+    echo "[!] Latest Antidetect is already downloaded. To download again, please delete C:\Antidetect_7.3.1.zip file"
 ) else (
     echo "[*] Downloading latest Antidetect"
     powershell iwr -outf C:\Antidetect_7.3.1.zip https://s3-us-west-2.amazonaws.com/mvp-store/Antidetect_7.3.1.zip
 )
 
-if exist C:\Antidetect (
-    echo "[!] Antidetect is already installed. Delete C:\Antidetect to reinstall"
-    echo "[!] This will also regenerate antidetect config"
-
-) else (
-    echo "[*] Installing Antidetect"
-    powershell Expand-Archive -force C:\Antidetect_7.3.1.zip -DestinationPath C:\
-)
+echo "[*] Installing Antidetect"
+powershell Copy-Item C:\Antidetect\antidetect.conf -Destination C:\antidetect.conf
+powershell Expand-Archive -force C:\Antidetect_7.3.1.zip -DestinationPath C:\
+powershell Copy-Item C:\antidetect.conf -Destination C:\Antidetect\antidetect.conf
 
 echo "[*] Adding shortcuts to desktop"
-
 powershell iwr https://adsrental.com/static/antidetect/browser.exe -outf C:\Users\Administrator\Desktop\Browser.exe
 powershell Copy-Item C:/Antidetect/variables.conf -Destination C:\Users\Administrator\Desktop\variables.conf
 
