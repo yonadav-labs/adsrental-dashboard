8f957d502d3beef2f0be355cc6315e4c5b7c6dc9
Vlad Emelianov <volshebnyi@gmail.com>
2018-06-12 13:06:18 -0400

    Fixed links

--- adsrental/admin/bundler_lead_stat_admin.py
+++ adsrental/admin/bundler_lead_stat_admin.py
@@ -33,70 +33,70 @@ class BundlerLeadStatsAdmin(admin.ModelAdmin):
     change_list_template = 'admin/change_list_total.html'
 
     def in_progress_total_field(self, obj):
-        return mark_safe('<a href="{url}?status=In-Progress&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&status=In-Progress&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_lead_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.in_progress_total,
         ))
 
     def in_progress_offline_field(self, obj):
-        return mark_safe('<a href="{url}?status=In-Progress&online=offline&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&status=In-Progress&online=offline&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.in_progress_offline,
         ))
 
     def in_progress_wrong_pw_field(self, obj):
-        return mark_safe('<a href="{url}?status=In-Progress&wrong_password=yes&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&status=In-Progress&wrong_password=yes&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.in_progress_wrong_pw,
         ))
 
     def in_progress_security_checkpoint_field(self, obj):
-        return mark_safe('<a href="{url}?status=In-Progress&security_checkpoint=yes&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&status=In-Progress&security_checkpoint=yes&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.in_progress_security_checkpoint,
         ))
 
     def autobans_last_30_days_field(self, obj):
-        return mark_safe('<a href="{url}?auto_ban=yes&banned_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&auto_ban=yes&banned_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.autobans_last_30_days,
         ))
 
     def other_bans_last_30_days_field(self, obj):
-        return mark_safe('<a href="{url}?auto_ban=no&banned_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&auto_ban=no&banned_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.bans_last_30_days - obj.autobans_last_30_days,
         ))
 
     def qualified_today_field(self, obj):
-        return mark_safe('<a href="{url}?qualified_date=today&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&qualified_date=today&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.qualified_today,
         ))
 
     def qualified_yesterday_field(self, obj):
-        return mark_safe('<a href="{url}?qualified_date=yesterday&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&qualified_date=yesterday&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.qualified_yesterday,
         ))
 
     def qualified_last_30_days_field(self, obj):
-        return mark_safe('<a href="{url}?qualified_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&qualified_date=last_30_days&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.qualified_last_30_days,
         ))
 
     def qualified_total_field(self, obj):
-        return mark_safe('<a href="{url}?status=Qualified&bundler={bundler_id}">{value}</a>'.format(
+        return mark_safe('<a href="{url}?account_type__exact=Facebook&status=Qualified&bundler={bundler_id}">{value}</a>'.format(
             url=reverse('admin:adsrental_leadaccount_changelist'),
             bundler_id=obj.bundler_id,
             value=obj.qualified_total,
