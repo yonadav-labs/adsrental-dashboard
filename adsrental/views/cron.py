2bfb4840f1daae4f42bff543a998d5003d39eeb5
Vlad Emelianov <volshebnyi@gmail.com>
2018-06-13 10:38:52 -0400

    Add MAX_ESSENTIAL_RUNNING

--- adsrental/views/cron.py
+++ adsrental/views/cron.py
@@ -499,6 +499,7 @@ class CheckEC2View(View):
     '''
     Check :model:`adsrental.EC2Instance` if they have action RDP session, stops them otherwise.
     '''
+    MAX_ESSENTIAL_RUNNING = 2
 
     def get(self, request):
         online_ec2s = []
@@ -527,9 +528,11 @@ class CheckEC2View(View):
 
         ec2_instances = EC2Instance.objects.filter(
             last_rdp_start__lt=now - datetime.timedelta(minutes=15),
+            status=EC2Instance.STATUS_RUNNING,
             rpid__isnull=False,
             is_essential=True,
         )
+        essential_running_counter = 0
         results = pool.map(lambda x: [x, x.is_rdp_session_active()], ec2_instances)
         for ec2_instance, is_rdp_session_active in results:
             if is_rdp_session_active:
@@ -537,8 +540,11 @@ class CheckEC2View(View):
                 ec2_instance.save()
                 online_essential_ec2s.append(ec2_instance.rpid)
             else:
+                essential_running_counter += 1
                 unassigned_essential_ec2s.append(ec2_instance.rpid)
                 ec2_instance.unassign_essential()
+                if essential_running_counter > self.MAX_ESSENTIAL_RUNNING:
+                    ec2_instance.stop()
 
         if stopping_instances:
             ec2_client.stop_instances(InstanceIds=[ec2.instance_id for ec2 in stopping_instances])
