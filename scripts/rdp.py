ffc8d54cb01d7c8b838798d39889dfce8ef8a95a
Vlad Emelianov <volshebnyi@gmail.com>
2018-06-15 14:08:52 -0400

    New RDP script

--- scripts/rdp.py
+++ scripts/rdp.py
@@ -3,6 +3,8 @@ import os
 import argparse
 from pathlib import Path
 import base64
+import pymysql
+
 
 from Crypto.Cipher import DES3
 import boto3
@@ -46,8 +48,26 @@ sharesmartcard=0
 parser = argparse.ArgumentParser(description='Connect to RDP by RPID or email.')
 parser.add_argument('--rpid', help='RPID for Raspberry Pi')
 parser.add_argument('--email', help='Lead email')
-parser.add_argument('-p', '--password', help='Passowrd', default='AdsInc18')
+parser.add_argument('--id', help='EC2Instance Local ID')
+parser.add_argument('-p', '--password', help='Password', default='')
 args = parser.parse_args()
+instance_id = None
+
+if args.id:
+    conn = pymysql.connect(host='localhost', port=23306, user='root', db='adsrental')
+    cursor = conn.cursor()
+    cursor.execute("SELECT rpid, password, instance_id FROM adsrental_ec2instance where id='{}'".format(args.id))
+    for row in cursor:
+        args.rpid = row[0]
+        args.password = row[1]
+        instance_id = row[2]
+
+if not args.password:
+    conn = pymysql.connect(host='localhost', port=23306, user='root', db='adsrental')
+    cursor = conn.cursor()
+    cursor.execute("SELECT password FROM adsrental_ec2instance where rpid='{}'".format(args.rpid))
+    for row in cursor:
+        args.password = row[0]
 
 secret = base64.b64decode('tCViJQOwkdqG88Ww9WpsVwc7CXBiwo89+5c0Y1awrgo=')
 password = args.password.encode('ascii')
@@ -67,15 +87,18 @@ if args.rpid:
     else:
         rpid = args.rpid
 
-if not args.rpid and not args.email:
-    rpid = input('Enter RPID: ')
-
 boto_client = boto3.Session(
     aws_access_key_id='AKIAJ3IUVXDRV2ZS2QLQ',
     aws_secret_access_key='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',
 ).resource('ec2', region_name='us-west-2')
 
-if args.email:
+if instance_id:
+    instances = boto_client.instances.filter(
+        InstanceIds=[
+            instance_id,
+        ],
+    )
+elif args.email:
     instances = boto_client.instances.filter(
         Filters=[
             {
@@ -100,6 +123,7 @@ if not instances:
 
 instance = None
 for i in instances:
+    print(i, i.state['Name'])
     if i.state['Name'] == 'running':
         instance = i
 
