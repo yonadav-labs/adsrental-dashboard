ce7fc9513fc2f281934b2149bf9cdb1d572a3868
Vlad Emelianov <volshebnyi@gmail.com>
2018-06-25 00:31:03 -0400

    Add MLA wallpaper

--- adsrental/static/mla/install_mla.bat
+++ adsrental/static/mla/install_mla.bat
@@ -2,6 +2,11 @@
 
 if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit)
 
+echo [*] Installing MLA wallpaper
+powershell iwr https://adsrental.com/static/images/mla_wp.png -OutFile C:\Users\Administrator\mla_wp.png
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\Administrator\mla_wp.png /f
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+
 if exist C:\mla_2.1.4.zip (
     echo [!] Latest MLA is already downloaded. To download again, please delete C:\mla_2.1.4.zip file
 ) else (
