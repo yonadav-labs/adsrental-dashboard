9bc99fbdab98f7c3b2dd19b9a1886311852c5ab7
Vlad Emelianov <volshebnyi@gmail.com>
2018-07-10 16:06:42 -0400

    Create esentials

--- adsrental/views/rdp.py
+++ adsrental/views/rdp.py
@@ -81,31 +81,25 @@ class RDPConnectView(View):
         force = request.GET.get('force', '') == 'true'
         action = request.GET.get('action', '')
         is_ready = False
-        ec2_instance = EC2Instance.get_by_rpid(rpid)
-        if not ec2_instance:
-            ec2_instance = EC2Instance.objects.filter(is_essential=True, rpid__isnull=True).first()
-            if ec2_instance:
-                lead = Lead.objects.filter(raspberry_pi__rpid=rpid).first()
-                if not lead:
-                    messages.warning(request, 'This RPID has no assigned lead.')
-                    return render(request, 'rdp_connect.html', dict(
-                        rpid=rpid,
-                        ec2_instance=None,
-                        check_connection=False,
-                        is_ready=is_ready,
-                        netstat_url=request.build_absolute_uri(reverse('ec2_ssh_get_netstat', kwargs=dict(rpid=rpid))),
-                    ))
 
-                ec2_instance.assign_essential(rpid, lead)
-
-        if not ec2_instance:
+        lead = Lead.objects.filter(raspberry_pi__rpid=rpid).first()
+        if not lead:
+            messages.warning(request, 'This RPID has no assigned lead.')
             return render(request, 'rdp_connect.html', dict(
                 rpid=rpid,
-                ec2_instance=ec2_instance,
+                ec2_instance=None,
                 check_connection=False,
                 is_ready=is_ready,
                 netstat_url=request.build_absolute_uri(reverse('ec2_ssh_get_netstat', kwargs=dict(rpid=rpid))),
             ))
+
+        ec2_instance = EC2Instance.get_by_rpid(rpid)
+        if not ec2_instance:
+            ec2_instance = EC2Instance.objects.filter(is_essential=True, rpid__isnull=True).first()
+            if not ec2_instance:
+                ec2_instance = EC2Instance.launch_essential()
+                messages.info(request, 'New Essential EC2 instance has been launched.')
+            ec2_instance.assign_essential(rpid, lead)
 
         if ec2_instance.is_essential:
             messages.success(request, 'Using essential EC2.')
