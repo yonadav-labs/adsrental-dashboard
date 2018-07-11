670c3337fce5db5b03d3479bfbb12a5ba9d484e6
Vlad Emelianov <volshebnyi@gmail.com>
2018-07-11 18:53:03 -0400

    Rertry creating reverse tunnel

--- adsrental/views/ec2_ssh.py
+++ adsrental/views/ec2_ssh.py
@@ -11,37 +11,34 @@ from adsrental.models.ec2_instance import EC2Instance, SSHConnectException
 
 class StartReverseTunnelView(View):
     'Start reverse tunnel from EC2. Used as a fallback if RaspberryPi cannot created it by itself'
+    MAX_ATTEMPTS = 10
     def get(self, request, rpid):
         'Start reverse tunnel from EC2. Used as a fallback if RaspberryPi cannot created it by itself'
         ec2_instance = EC2Instance.get_by_rpid(rpid)
         if not ec2_instance or not ec2_instance.is_running():
             return JsonResponse(dict(result=False))
 
-        try:
-            ec2_instance.ssh_execute('ssh -N -D 3808 -p 2046 pi@localhost')
-        except SSHConnectException:
-            return JsonResponse(dict(result=False))
-        return JsonResponse(dict(result=True))
+        tunnel_up = False
+        attempts = 0
+        netstat_output = True
+
+        while not tunnel_up and attempts < MAX_ATTEMPTS and netstat_output:
+            attempts += 1
+            netstat_output = ''
+            try:
+                ec2_instance.ssh_execute('ssh -N -D 3808 -p 2046 pi@localhost')
+                netstat_output = ec2_instance.ssh_execute('netstat -an') or ''
+                if ec2_instance.REVERSE_TUNNEL_RE.search(netstat_output):
+                    tunnel_up = True
+            except SSHConnectException:
+                pass
+        return JsonResponse(dict(
+            result=tunnel_up,
+            attempts=attempts,
+        ))
 
 
 class GetNetstatView(View):
-    def add_log(self, request, rpid, message):
-        ip_address = request.META.get('REMOTE_ADDR')
-        now = timezone.localtime(timezone.now())
-        log_path = os.path.join(
-            settings.RASPBERRY_PI_LOG_PATH,
-            rpid,
-            '{}.log'.format(now.strftime('%Y%m%d')),
-        )
-        if not os.path.exists(os.path.dirname(log_path)):
-            os.makedirs(os.path.dirname(log_path))
-        with open(log_path, 'a') as log_file:
-            log_file.write('{ts}: {ip}: {message}\n'.format(
-                ts=now.strftime(settings.SYSTEM_DATETIME_FORMAT),
-                ip=ip_address,
-                message=message,
-            ))
-
     def get(self, request, rpid):
         'Get netstat output from EC2. Used as a fallback if RaspberryPi cannot get it by itself'
         rpid = rpid.strip()
