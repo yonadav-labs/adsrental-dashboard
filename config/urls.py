37936121f4912dd922bb79754d6eb58530cd070d
Vlad Emelianov <volshebnyi@gmail.com>
2018-07-11 18:05:03 -0400

    COnverts urls

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.conf.urls import handler404, handler500  # pylint: disable=W0611
 import debug_toolbar
@@ -8,14 +8,13 @@ from adsrental.views.errors import Error404View, Error500View
 admin.site.site_header = 'Adsrental Administration'
 
 urlpatterns = [
-    url(r'^app/admin_tools/', include('admin_tools.urls')),
-    # url(r'^admin/', admin.site.urls),
-    url(r'^', include('adsrental.urls')),
-    url(r'^app/admin/', admin.site.urls),
-    url(r'^app/', include('adsrental.urls')),
-    url(r'^app/admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^__debug__/', include(debug_toolbar.urls)),
-    url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
+    path('app/admin_tools/', include('admin_tools.urls')),
+    path('', include('adsrental.urls')),
+    path('app/admin/', admin.site.urls),
+    path('app/', include('adsrental.urls')),
+    path('app/admin/doc/', include('django.contrib.admindocs.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
+    path('robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
 ]
 
 handler404 = Error404View.as_view()  # noqa: F811
