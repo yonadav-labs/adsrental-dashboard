e9c63584533b58b4fae9f721f2fc27a2e2046501
Vlad Emelianov <volshebnyi@gmail.com>
2018-07-27 14:39:55 -0400

    Add preview

--- adsrental/urls/bookkeeper.py
+++ adsrental/urls/bookkeeper.py
@@ -2,9 +2,11 @@ from django.urls import path
 
 from adsrental.views.bookkeeper.reports_list import BookkeeperReportsListView
 from adsrental.views.bookkeeper.report_html import BookkeeperReportHTMLView
+from adsrental.views.bookkeeper.report_preview import BookkepperReportPreviewView
 
 
 urlpatterns = [
     path('reports/', BookkeeperReportsListView.as_view(), name='bookkeeper_reports_list'),
     path('reports/<int:report_id>/', BookkeeperReportHTMLView.as_view(), name='bookkeeper_report_html'),
+    path('reports/preview/', BookkepperReportPreviewView.as_view(), name='bookkeeper_report_preview'),
 ]
---
+++ adsrental/views/bookkeeper/report_preview.py
@@ -0,0 +1,231 @@
+import datetime
+import json
+import io
+import decimal
+
+from django.shortcuts import render, redirect
+from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.utils import timezone
+from django.template.loader import render_to_string
+from django.http import HttpResponse
+from django.core.files.base import ContentFile
+import xhtml2pdf.pisa as pisa
+
+from adsrental.models.lead_account import LeadAccount
+from adsrental.models.bundler import Bundler
+from adsrental.models.bundler_payments_report import BundlerPaymentsReport
+
+
+class BookkepperReportPreviewView(View):
+    def get_account_type_stats(self, bundler, end_date, account_type, child=False):
+        total = decimal.Decimal('0.00')
+        final_total = decimal.Decimal('0.00')
+        chargeback_total = decimal.Decimal('0.00')
+
+        lead_accounts = LeadAccount.objects.filter(
+            lead__bundler=bundler,
+            pay_check=True,
+            account_type=account_type,
+            active=True,
+        ).order_by('created').select_related('lead__bundler', 'lead', 'lead__raspberry_pi')
+
+        entries = []
+        for lead_account in lead_accounts:
+            payment = lead_account.get_bundler_payment(bundler)
+            parent_payment = lead_account.get_parent_bundler_payment(bundler)
+            lead_account.parent_payment = parent_payment
+            if child:
+                lead_account.payment = parent_payment
+            else:
+                lead_account.payment = payment
+
+        for lead_account in lead_accounts:
+            payment = lead_account.payment
+            parent_payment = lead_account.parent_payment
+            if payment > 0 or parent_payment > 0:
+                final_total += payment
+                total += payment
+                entries.append(lead_account)
+
+        if not child:
+            for lead_account in lead_accounts:
+                payment = lead_account.payment
+                if payment < 0 and final_total + payment >= 0:
+                    final_total += payment
+                    chargeback_total += -payment
+                    entries.append(lead_account)
+
+        children_stats = []
+        for child_bundler in Bundler.objects.filter(parent_bundler=bundler):
+            child_stats = self.get_account_type_stats(child_bundler, end_date, account_type, child=True)
+            children_stats.append(child_stats)
+
+        children_stats.sort(key=lambda x: x['total'], reverse=True)
+        children_total = decimal.Decimal('0.00')
+        for child_stats in children_stats:
+            children_total += child_stats['total']
+
+        return dict(
+            bundler=bundler,
+            entries=entries,
+            children_stats=children_stats,
+            total=total,
+            children_total=children_total,
+            final_total=final_total,
+            chargeback_total=chargeback_total,
+        )
+
+    @method_decorator(login_required)
+    def get(self, request):
+        bundlers = []
+        if request.user.bundler:
+            bundlers = [request.user.bundler]
+
+        bundlers = Bundler.objects.all()
+
+        yesterday = (timezone.now() - datetime.timedelta(days=1)).date()
+        bundlers_data = []
+        total = decimal.Decimal('0.00')
+        total_google = decimal.Decimal('0.00')
+        total_facebook = decimal.Decimal('0.00')
+        total_amazon = decimal.Decimal('0.00')
+        total_chargeback = decimal.Decimal('0.00')
+
+        for bundler in bundlers:
+            facebook_stats = self.get_account_type_stats(bundler, yesterday, LeadAccount.ACCOUNT_TYPE_FACEBOOK)
+            google_stats = self.get_account_type_stats(bundler, yesterday, LeadAccount.ACCOUNT_TYPE_GOOGLE)
+            amazon_stats = self.get_account_type_stats(bundler, yesterday, LeadAccount.ACCOUNT_TYPE_AMAZON)
+            bundler_total = facebook_stats['final_total'] + google_stats['final_total'] + amazon_stats['final_total']
+            for child_stats in facebook_stats['children_stats']:
+                bundler_total += child_stats['total']
+            for child_stats in google_stats['children_stats']:
+                bundler_total += child_stats['total']
+            for child_stats in amazon_stats['children_stats']:
+                bundler_total += child_stats['total']
+
+            bundlers_data.append(dict(
+                bundler=bundler,
+                facebook_entries=facebook_stats['entries'],
+                facebook_total=facebook_stats['total'],
+                facebook_chargeback_total=facebook_stats['chargeback_total'],
+                facebook_final_total=facebook_stats['final_total'],
+                facebook_children_stats=facebook_stats['children_stats'],
+                facebook_children_total=facebook_stats['children_total'],
+                google_entries=google_stats['entries'],
+                google_total=google_stats['total'],
+                google_chargeback_total=google_stats['chargeback_total'],
+                google_final_total=google_stats['final_total'],
+                google_children_stats=google_stats['children_stats'],
+                google_children_total=google_stats['children_total'],
+                amazon_entries=amazon_stats['entries'],
+                amazon_total=amazon_stats['total'],
+                amazon_chargeback_total=amazon_stats['chargeback_total'],
+                amazon_final_total=amazon_stats['final_total'],
+                amazon_children_stats=amazon_stats['children_stats'],
+                amazon_children_total=amazon_stats['children_total'],
+                total=bundler_total,
+            ))
+
+        for data in bundlers_data:
+            total += data['total']
+            total_google += data['google_total'] + data['google_children_total']
+            total_facebook += data['facebook_total'] + data['facebook_children_total']
+            total_amazon += data['amazon_total'] + data['amazon_children_total']
+            total_chargeback += data['facebook_chargeback_total'] + data['google_chargeback_total'] + data['amazon_chargeback_total']
+
+        bundlers_data.sort(key=lambda x: x['total'], reverse=True)
+
+        if request.GET.get('save'):
+            html = render_to_string(
+                'bundler_payments_pdf.html',
+                dict(
+                    user=request.user,
+                    bundlers_data=bundlers_data,
+                    end_date=yesterday,
+                    total=total,
+                    show_bundler_name=request.user.is_superuser,
+                    allow_change=request.user.is_superuser,
+                    pdf=True,
+                    total_google=total_google,
+                    total_facebook=total_facebook,
+                    total_amazon=total_amazon,
+                    total_chargeback=total_chargeback,
+                ),
+                request=request,
+            )
+            pdf_stream = io.BytesIO()
+            pisa.pisaDocument(io.BytesIO(html.encode('UTF-8')), dest=pdf_stream)
+            pdf_stream.seek(0)
+
+            report_data = []
+            for data in bundlers_data:
+                report_data.append(dict(
+                    bundler=data['bundler'].id,
+                    facebook_entries=[{
+                        'id': i.id,
+                        'payment': str(i.payment),
+                        'parent_payment': str(i.parent_payment),
+                    } for i in data['facebook_entries']],
+                    google_entries=[{
+                        'id': i.id,
+                        'payment': str(i.payment),
+                        'parent_payment': str(i.parent_payment),
+                    } for i in data['google_entries']],
+                    amazon_entries=[{
+                        'id': i.id,
+                        'payment': str(i.payment),
+                        'parent_payment': str(i.parent_payment),
+                    } for i in data['amazon_entries']],
+                ))
+
+            report = BundlerPaymentsReport(
+                date=yesterday,
+                data=json.dumps(report_data),
+                html=html,
+                pdf=ContentFile(pdf_stream.read(), name='{}.pdf'.format(yesterday)),
+            )
+            report.save()
+
+            if request.GET.get('mark', '') == 'true':
+                report.mark()
+
+            return redirect('admin:adsrental_bundlerpaymentsreport_changelist')
+
+        if request.GET.get('pdf'):
+            html = render_to_string(
+                'bundler_payments_pdf.html',
+                dict(
+                    user=request.user,
+                    bundlers_data=bundlers_data,
+                    end_date=yesterday,
+                    total=total,
+                    show_bundler_name=request.user.is_superuser,
+                    pdf=True,
+                    allow_change=request.user.is_superuser,
+                    total_google=total_google,
+                    total_facebook=total_facebook,
+                    total_amazon=total_amazon,
+                    total_chargeback=total_chargeback,
+                ),
+                request=request,
+            )
+            response = io.BytesIO()
+            pisa.pisaDocument(io.BytesIO(html.encode('UTF-8')), response)
+            return HttpResponse(response.getvalue(), content_type='application/pdf')
+
+        response = render(request, 'bookkeeper/report_preview.html', dict(
+            user=request.user,
+            bundlers_data=bundlers_data,
+            end_date=yesterday,
+            total=total,
+            show_bundler_name=request.user.is_superuser,
+            allow_change=request.user.is_superuser,
+            total_google=total_google,
+            total_facebook=total_facebook,
+            total_amazon=total_amazon,
+            total_chargeback=total_chargeback,
+        ))
+
+        return response
