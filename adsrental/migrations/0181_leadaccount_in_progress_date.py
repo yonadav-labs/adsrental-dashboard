fa1d1dabfefefc35c2a9a5c4db5574f18fc795ff
Vlad Emelianov <volshebnyi@gmail.com>
2018-07-30 14:41:10 -0400

    Fix migrations

--- adsrental/migrations/0181_leadaccount_in_progress_date.py
+++ adsrental/migrations/0181_leadaccount_in_progress_date.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('adsrental', '0179_auto_20180726_1052'),
+        ('adsrental', '0180_vultrinstance'),
     ]
 
     operations = [
