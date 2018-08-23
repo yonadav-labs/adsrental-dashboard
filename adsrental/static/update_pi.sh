fd7ff0eefdacb512fc968d5ea44ecb4bfd2c42f1
Vlad Emelianov <volshebnyi@gmail.com>
2018-08-23 09:30:29 -0400

    Add debug

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -7,8 +7,10 @@ sudo apt install jq
 
 CONNECTION_DATA=$(curl -s "http://adsrental.com/rpi/${RASPBERRYPI_ID}/connection_data/")
 IS_PROXY_TUNNEL=`echo "$CONNECTION_DATA" | jq -r '.is_proxy_tunnel'`
+${HOME}/new-pi/client_log.sh "Response: $CONNECTION_DATA $IS_PROXY_TUNNEL"
 
 if [[ "${IS_PROXY_TUNNEL}" == "true" ]]; then
+	${HOME}/new-pi/client_log.sh "Updating to proxy tunnel..."
     cd /home/pi/new-pi/
     curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.0.zip > pi_patch.zip
     unzip -o pi_patch.zip
