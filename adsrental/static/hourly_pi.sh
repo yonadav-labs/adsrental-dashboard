a5b88e2aa2927048faeb79310e27f18a56b4779b
Vlad Emelianov <volshebnyi@gmail.com>
2018-08-23 09:23:08 -0400

    No hourly

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -7,13 +7,13 @@ ${HOME}/new-pi/client_log.sh "Hourly script for ${RASPBERRYPI_ID}"
 
 sudo apt install jq
 
-CONNECTION_DATA=$(curl -s "http://adsrental.com/rpi/${RASPBERRYPI_ID}/connection_data/")
-IS_PROXY_TUNNEL=`echo "$CONNECTION_DATA" | jq -r '.is_proxy_tunnel'`
+# CONNECTION_DATA=$(curl -s "http://adsrental.com/rpi/${RASPBERRYPI_ID}/connection_data/")
+# IS_PROXY_TUNNEL=`echo "$CONNECTION_DATA" | jq -r '.is_proxy_tunnel'`
 
-if [[ "${IS_PROXY_TUNNEL}" == "true" ]]; then
-    cd /home/pi/new-pi/
-    curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.0.zip > pi_patch.zip
-    unzip -o pi_patch.zip
-    sudo sync
-    ${HOME}/new-pi/client_log.sh "Installed 2.0.0"
-fi
+# if [[ "${IS_PROXY_TUNNEL}" == "true" ]]; then
+#     cd /home/pi/new-pi/
+#     curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.0.zip > pi_patch.zip
+#     unzip -o pi_patch.zip
+#     sudo sync
+#     ${HOME}/new-pi/client_log.sh "Installed 2.0.0"
+# fi
