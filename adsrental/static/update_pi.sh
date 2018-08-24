8ecf3e9eb89d9a362a762d16464237be64484a4c
Vlad Emelianov <volshebnyi@gmail.com>
2018-08-24 15:21:26 -0400

    FIx update

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -6,21 +6,18 @@ sudo apt-get install -y jq
 
 RASPBERRYPI_ID="`head -n 1 ${HOME}/rpid.conf`"
 CONNECTION_DATA=$(curl -s "http://adsrental.com/rpi/${RASPBERRYPI_ID}/connection_data/")
-IS_PROXY_TUNNEL=`echo "$CONNECTION_DATA" | jq -r '.is_proxy_tunnel'`
-${HOME}/new-pi/client_log.sh "Response: $CONNECTION_DATA $IS_PROXY_TUNNEL"
+IS_BETA=`echo "$CONNECTION_DATA" | jq -r '.is_beta'`
+${HOME}/new-pi/client_log.sh "Response: $CONNECTION_DATA Beta: $IS_BETA"
 
-if [[ "${IS_PROXY_TUNNEL}" == "true" ]]; then
-	${HOME}/new-pi/client_log.sh "Updating to proxy tunnel..."
-    cd /home/pi/new-pi/
-    curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.0.zip > pi_patch.zip
-    unzip -o pi_patch.zip
-    sudo sync
-    ${HOME}/new-pi/client_log.sh "Installed!"
-    exit;
+VERSION = "1.1.10"
+if [[ "${IS_BETA}" == "true" ]]; then
+    VERSION="2.0.0"
 fi
 
-curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_1.1.10.zip > pi_patch.zip
+cd /home/pi/new-pi/
+curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_${VERSION}.zip > pi_patch.zip
 unzip -o pi_patch.zip
 cat /home/pi/new-pi/crontab.txt | crontab
 sudo sync
+${HOME}/new-pi/client_log.sh "Installed ${VERSION}!"
 # sudo reboot
