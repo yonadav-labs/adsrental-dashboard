ec4019af673ebe6cd5bee7617b37740e5993d242
Vlad Emelianov <volshebnyi@gmail.com>
2018-08-30 20:11:14 -0400

    Bump Rpi version to 2.0.1

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -22,10 +22,10 @@ IS_BETA=`echo "$CONNECTION_DATA" | jq -r '.is_beta'`
 ${HOME}/new-pi/client_log.sh "Response: $CONNECTION_DATA Beta: $IS_BETA"
 
 # setings.RASPBERRY_PI_VERSION
-VERSION="2.0.0"
+VERSION="2.0.1"
 if [[ "${IS_BETA}" == "true" ]]; then
     # setings.BETA_RASPBERRY_PI_VERSION
-    VERSION="2.0.0"
+    VERSION="2.0.1"
 fi
 
 cd /home/pi/new-pi/
