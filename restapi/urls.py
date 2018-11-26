265506bcc0333069a047b452debcc56a5a263c2c
Vlad Emelianov <volshebnyi@gmail.com>
2018-11-26 11:42:52 -0400

    Add token auth

--- restapi/urls.py
+++ restapi/urls.py
@@ -2,6 +2,7 @@ from django.urls import include, path
 from rest_framework.schemas import get_schema_view
 
 from restapi.routers import main_router
+from restapi.views import login_view
 
 schema_view = get_schema_view(title='Adsrental API')
 
@@ -9,4 +10,5 @@ urlpatterns = [
     path('schema/', schema_view),
     path('', include(main_router.urls)),
     path('auth/', include('rest_framework.urls')),
+    path('login/', login_view),
 ]
