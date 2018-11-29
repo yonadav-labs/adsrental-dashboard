7797b600cc98f4f2de704e969b87b954c8712795
Vlad Emelianov <volshebnyi@gmail.com>
2018-11-29 11:02:20 -0400

    Add filtering

--- restapi/viewsets.py
+++ restapi/viewsets.py
@@ -10,18 +10,22 @@ from adsrental.models.bundler import Bundler
 class LeadViewSet(viewsets.ModelViewSet):
     queryset = Lead.objects.all()
     serializer_class = LeadSerializer
+    filter_fields = ('first_name', 'last_name', 'status', 'email', )
 
 
 class LeadAccountViewSet(viewsets.ModelViewSet):
     queryset = LeadAccount.objects.all()
     serializer_class = LeadAccountSerializer
+    filter_fields = ('lead__first_name', 'lead__last_name', 'lead__email', 'adsdb_account_id', 'status', 'account_type', 'username', )
 
 
 class RaspberryPiViewSet(viewsets.ModelViewSet):
     queryset = RaspberryPi.objects.all()
     serializer_class = RaspberryPiSerializer
+    filter_fields = ('rpid', )
 
 
 class BundlerViewSet(viewsets.ModelViewSet):
     queryset = Bundler.objects.all()
     serializer_class = BundlerSerializer
+    filter_fields = ('name', 'utm_source', 'adsdb_id', )
