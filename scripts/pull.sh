ec779907c80d82b9e734576f9453876b75c1421e
Vlad Emelianov <volshebnyi@gmail.com>
2018-12-25 14:17:36 -0400

    Fix pull script

--- scripts/pull.sh
+++ scripts/pull.sh
@@ -2,14 +2,13 @@
 
 CONFIG="docker-compose.prod.yml"
 
-if [ "$1" = "migrate" ]; then
-    cd ~/migrate/dashboard/
-    git pull
-    docker-compose -f ${CONFIG} build
-    docker-compose -f ${CONFIG} run web python manage.py migrate adsrental
-fi
 
 cd ~/dashboard/
 git pull
 docker-compose -f ${CONFIG} build
+
+if [ "$1" = "migrate" ]; then
+    docker-compose -f ${CONFIG} run web python manage.py migrate adsrental
+fi
+
 docker-compose -f ${CONFIG} up --remove-orphans -d
