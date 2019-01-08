5623464f2c9f865df6fd63de01fd17a3a8ad82cb
Vlad Emelianov <volshebnyi@gmail.com>
2019-01-08 11:35:52 -0400

    FIx timeout

--- adsrental/models/raspberry_pi.py
+++ adsrental/models/raspberry_pi.py
@@ -313,7 +313,7 @@ class RaspberryPi(models.Model):
                 http=self.get_proxy_connection_string(),
                 https=self.get_proxy_connection_string(),
             ),
-            timeout=5,
+            timeout=2,
         )
 
     def get_unique_ips(self) -> typing.List[str]:
