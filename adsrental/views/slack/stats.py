6b8e6e181184ab9dfe2c04836042a792709b9884
Vlad Emelianov <volshebnyi@gmail.com>
2019-04-12 12:43:46 -0400

    Add title link

--- adsrental/views/slack/stats.py
+++ adsrental/views/slack/stats.py
@@ -3,6 +3,7 @@ import datetime
 from django.views import View
 from django.http import JsonResponse
 from django.utils import timezone
+from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
@@ -14,8 +15,9 @@ from adsrental.models.lead_account import LeadAccount
 @method_decorator(csrf_exempt, name='dispatch')
 class StatsView(View):
     FIELDS_LIMIT = 10
+    LINK_HOST = 'https://adsrental.com'
 
-    def lead_accounts_to_attachment(self, lead_accounts, title, color):
+    def lead_accounts_to_attachment(self, lead_accounts, title, color, title_link=None):
         fields = []
         for lead_account in lead_accounts[:self.FIELDS_LIMIT]:
             fields.append({
@@ -29,49 +31,63 @@ class StatsView(View):
                 "short": False
             })
 
-        return {
+        result = {
             "title": f"{lead_accounts.count()} {title}",
             "color": color,
             "fields": fields
         }
+        if title_link:
+            result['title_link'] = title_link
+        return result
 
     def dispatch(self, request, *args, **kwargs):
+        # import json
+        # return JsonResponse({'text': json.dumps(request.POST)})
         slack_tag = request.POST.get('user_name', '')
+        if request.POST.get('text'):
+            slack_tag = request.POST.get('text').replace('@', '')
+
         now = timezone.localtime(timezone.now())
         try:
             bundler = Bundler.objects.get(slack_tag=slack_tag)
         except Bundler.DoesNotExist:
-            return JsonResponse({'text': f'Cannot find bundler for `{slack_tag}` user'})
+            return JsonResponse({'text': f'Cannot find bundler for <@{slack_tag}> (`{slack_tag}`) user'})
 
         offline_facebook_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
-            account_type__in=LeadAccount.ACCOUNT_TYPES_FACEBOOK,
+            account_type=LeadAccount.ACCOUNT_TYPE_FACEBOOK,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             lead__raspberry_pi__last_seen__lt=now - datetime.timedelta(minutes=RaspberryPi.online_minutes_ttl),
         ).select_related('lead', 'lead__raspberry_pi')
         sec_check_facebook_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
-            account_type__in=LeadAccount.ACCOUNT_TYPES_FACEBOOK,
+            account_type=LeadAccount.ACCOUNT_TYPE_FACEBOOK,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             security_checkpoint_date__isnull=False,
         ).select_related('lead', 'lead__raspberry_pi')
         offline_2_hours_facebook_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
-            account_type__in=LeadAccount.ACCOUNT_TYPES_FACEBOOK,
+            account_type=LeadAccount.ACCOUNT_TYPE_FACEBOOK,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             lead__raspberry_pi__last_seen__lt=now - datetime.timedelta(hours=2),
         ).select_related('lead', 'lead__raspberry_pi')
 
         offline_google_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
             account_type=LeadAccount.ACCOUNT_TYPE_GOOGLE,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             lead__raspberry_pi__last_seen__lt=now - datetime.timedelta(minutes=RaspberryPi.online_minutes_ttl),
         ).select_related('lead', 'lead__raspberry_pi')
         sec_check_google_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
             account_type=LeadAccount.ACCOUNT_TYPE_GOOGLE,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             security_checkpoint_date__isnull=False,
         ).select_related('lead', 'lead__raspberry_pi')
         offline_2_hours_google_lead_accounts = LeadAccount.objects.filter(
             lead__bundler=bundler,
             account_type=LeadAccount.ACCOUNT_TYPE_GOOGLE,
+            status=LeadAccount.STATUS_IN_PROGRESS,
             lead__raspberry_pi__last_seen__lt=now - datetime.timedelta(hours=2),
         ).select_related('lead', 'lead__raspberry_pi')
 
@@ -81,18 +97,21 @@ class StatsView(View):
                 offline_facebook_lead_accounts,
                 title='offline Facebook accounts',
                 color='warning',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&raspberry_pi_status=offline&account_type=facebook",
             ))
         if sec_check_facebook_lead_accounts:
             attachments.append(self.lead_accounts_to_attachment(
                 sec_check_facebook_lead_accounts,
                 title=' Facebook accounts with a security checkpoint',
                 color='danger',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&security_checkpoint=yes&account_type=facebook",
             ))
         if offline_2_hours_facebook_lead_accounts:
             attachments.append(self.lead_accounts_to_attachment(
                 offline_2_hours_facebook_lead_accounts,
                 title='Facebook accounts that have been offline for 2 or more hours',
-                color='danger'
+                color='danger',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&raspberry_pi_status=offline&account_type=facebook",
             ))
 
         if offline_google_lead_accounts:
@@ -100,18 +119,21 @@ class StatsView(View):
                 offline_google_lead_accounts,
                 title='offline Google accounts',
                 color='warning',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&raspberry_pi_status=offline&account_type=google",
             ))
         if sec_check_google_lead_accounts:
             attachments.append(self.lead_accounts_to_attachment(
                 sec_check_google_lead_accounts,
                 title=' Google accounts with a security checkpoint',
                 color='danger',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&security_checkpoint=yes&account_type=google",
             ))
         if offline_2_hours_google_lead_accounts:
             attachments.append(self.lead_accounts_to_attachment(
                 offline_2_hours_google_lead_accounts,
                 title='Google accounts that have been offline for 2 or more hours',
-                color='danger'
+                color='danger',
+                title_link=f"{self.LINK_HOST}{reverse('dashboard')}?lead_status=In-Progress&raspberry_pi_status=offline&account_type=google",
             ))
 
         # raise ValueError(attachments)
