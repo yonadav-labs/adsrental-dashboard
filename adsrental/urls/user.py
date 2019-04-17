6bcd98f5eb5d6265034b8b7d240c69444c4c00fa
Vlad Emelianov <volshebnyi@gmail.com>
2019-04-17 11:03:40 -0400

    Fix timestamps

--- adsrental/urls/user.py
+++ adsrental/urls/user.py
@@ -11,7 +11,7 @@ urlpatterns = [  # pylint: disable=C0103
     path('login/', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
     path('stats/', UserStatsView.as_view(), name='user_stats'),
-    path('stats/<rpid>/', AdminUserStatsView.as_view(), name='admin_user_stats'),
     path('stats/timestamps/', UserTimestampsView.as_view(), name='user_timestamps'),
+    path('stats/rpid/<rpid>/', AdminUserStatsView.as_view(), name='admin_user_stats'),
     path('fix/<int:lead_account_issue_id>/', FixLeadAccountIssueView.as_view(), name='user_fix_lead_account_issue'),
 ]
