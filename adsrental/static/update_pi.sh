e117205aaf29823cf300ba1fbfbecaee5ee56dd8
Vlad Emelianov <volshebnyi@gmail.com>
2019-04-30 16:14:28 -0400

    Update to 2.0.8

--- adsrental/static/update_pi.sh
+++ adsrental/static/update_pi.sh
@@ -22,10 +22,10 @@ IS_BETA=`echo "$CONNECTION_DATA" | jq -r '.is_beta'`
 ${HOME}/new-pi/client_log.sh "Response: $CONNECTION_DATA Beta: $IS_BETA"
 
 # setings.RASPBERRY_PI_VERSION
-VERSION="2.0.7"
+VERSION="2.0.8"
 if [[ "${IS_BETA}" == "true" ]]; then
     # setings.BETA_RASPBERRY_PI_VERSION
-    VERSION="2.0.7"
+    VERSION="2.0.8"
 fi
 
 cd /home/pi/new-pi/
