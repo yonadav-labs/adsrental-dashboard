3c14c0154845f1b8fa43d0ebc2e43b0addb53429
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-10 15:40:56 -0400

    admin_unbuntest

--- e2e_tests/Admin_.js
+++ e2e_tests/Admin_.js
@@ -5,4 +5,8 @@ Scenario('success', async (I) =>{
 I.amOnPage('http://localhost:8443/admin/')
 I.see('Adsrental Administration')
 I.click('Master Report for Facebook Accounts')
-});
+I.fillField('select[name="action"]', 'Ban google account')
+I.click('Go')
+I.see('Choose reason to ban Google account')
+I.fillField('textarea[name="note"]need b')
+I.click('Ban')
