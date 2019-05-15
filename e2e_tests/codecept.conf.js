6f56b1b405c8403762c3440116dc2d0cff9b7c72
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-15 18:15:39 -0400

    FIx puppeteer waitForNavigation

--- e2e_tests/codecept.conf.js
+++ e2e_tests/codecept.conf.js
@@ -6,6 +6,7 @@ exports.config = {
       url: 'http://localhost',
       show: true,
       windowSize: '1920x1080',
+      waitForNavigation: "networkidle0",
       chrome: {
         args: ['--no-sandbox', '--window-size=1920,1080'],
         prefs: {
