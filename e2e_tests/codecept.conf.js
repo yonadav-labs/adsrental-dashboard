c63d5a8d8097133c59080108df537cba35747a81
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-16 11:12:23 -0400

    lifespan test

--- e2e_tests/codecept.conf.js
+++ e2e_tests/codecept.conf.js
@@ -6,6 +6,7 @@ exports.config = {
       url: 'http://localhost',
       show: true,
       windowSize: '1920x1080',
+      getPageTimeout: 60000,
       waitForNavigation: "networkidle0",
       chrome: {
         args: ['--no-sandbox', '--window-size=1920,1080'],
