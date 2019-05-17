50d88d0c8a0ad55dd7bfd2b5a8ac275bd558daff
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-17 13:38:20 -0400

    Add initial login helper

--- e2e_tests/steps_file.js
+++ e2e_tests/steps_file.js
@@ -1,11 +1,16 @@
-
 // in this file you can append custom step methods to 'I' object
 
-module.exports = function() {
+module.exports = function () {
   return actor({
 
     // Define custom steps here, use 'this' to access default methods of I.
     // It is recommended to place a general 'login' function here.
 
+    loginAsAdmin: function () {
+      this.see('Adsrental Administration')
+      this.fillField('input[name="username"]', 'volshebnyi@gmail.com')
+      this.fillField('input[name="password"]', 'team17')
+      this.click('Log in')
+    },
   });
 }
