05434fd8f408ea854b443d5c5c1713d48b4cab12
IT-corridor <it.corridor051@gmail.com>
2019-05-23 00:38:30 -0700

    feat: complete secret key generation

--- adsrental/urls/user.py
+++ adsrental/urls/user.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from adsrental.views.user.login import UserLoginView
+from adsrental.views.user.login import UserLoginView, UserSecretKeyView
 from adsrental.views.user.logout import UserLogoutView
 from adsrental.views.user.stats import UserStatsView, AdminUserStatsView
 from adsrental.views.user.actions import FixLeadAccountIssueView
@@ -10,6 +10,7 @@ from adsrental.views.user.timestamps import UserTimestampsView
 urlpatterns = [  # pylint: disable=C0103
     path('login/', UserLoginView.as_view(), name='user_login'),
     path('logout/', UserLogoutView.as_view(), name='user_logout'),
+    path('get-secret-key/', UserSecretKeyView.as_view(), name='user_get_secret_key'),
     path('stats/', UserStatsView.as_view(), name='user_stats'),
     path('stats/timestamps/', UserTimestampsView.as_view(), name='user_timestamps'),
     path('stats/rpid/<rpid>/', AdminUserStatsView.as_view(), name='admin_user_stats'),
