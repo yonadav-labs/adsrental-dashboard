6afdcfc13b606604816aea8a292412adbc5296f5
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-30 14:08:25 -0400

    Add extra behave helpers

---
+++ features/steps/helpers.py
@@ -0,0 +1,55 @@
+from behave import given, when, then  # pylint: disable=no-name-in-module
+from selenium import webdriver
+from behave_django.decorators import fixtures
+
+HOST = 'http://localhost:8000'
+
+
+def login_as_admin(driver, username, password):
+    driver.get(f"{HOST}/admin/login/")
+    driver.find_element_by_name('username').send_keys(username)
+    driver.find_element_by_name('password').send_keys(password)
+    driver.find_element_by_css_selector('input[type=submit]').click()
+
+
+@fixtures('test.json')
+@given('I am using initial database')
+def use_test_database(_context):
+    pass
+
+
+@given('I am logged in as an Admin')
+def logged_in_as_admin(context):
+    context.driver = webdriver.Chrome()
+    login_as_admin(context.driver, "volshebnyi@gmail.com", "team17")
+
+
+@given('I am logged in as user')
+def logged_in_as_user(context):
+    context.driver = webdriver.Chrome()
+    context.driver.get(f"{HOST}/usr/login/")
+    context.driver.find_element_by_name('first_name').send_keys('Vlad')
+    context.driver.find_element_by_name('last_name').send_keys('Emelianov')
+    context.driver.find_element_by_name('postal_code').send_keys('6348489')
+    context.driver.find_element_by_css_selector('input[type=submit]').click()
+
+
+@given('I am on main Admin Dashboard page')
+def on_main_admin_page(context):
+    context.driver.get(f"{HOST}/admin/")
+    context.driver.page_source.find('Dashboard')
+
+
+@when('I click link "{link_text}"')
+def click_link(context, link_text):
+    context.driver.find_element_by_link_text(link_text).click()
+
+
+@then('I should see text on page "{text}"')
+def see_test_on_page(context, text):
+    context.test.assertTrue(text in context.driver.page_source, f'Text "{text}" not found on page')
+
+
+@then('I should see "{text}" page title')
+def see_page_title(context, title):
+    context.test.assertEqual(context.driver.title, title)
