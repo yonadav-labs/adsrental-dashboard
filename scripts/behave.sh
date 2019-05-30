000f63b2df443d4be1c515aca24ded7244210edd
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-30 14:08:25 -0400

    Add extra behave helpers

--- scripts/behave.sh
+++ scripts/behave.sh
@@ -5,5 +5,5 @@ cd $ROOT_PATH
 # ENV=test python manage.py migrate adsrental
 # ENV=test python manage.py migrate
 ENV=test python manage.py loaddata adsrental/fixtures/test.json
-ENV=test python manage.py runserver_plus & > /dev/null
+# ENV=test python manage.py runserver_plus & > /dev/null
 ENV=test python manage.py behave $@
