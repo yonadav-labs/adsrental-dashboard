6ed8c984ee0de288f3d4d8d9cd1f625f82129ef9
Vlad Emelianov <volshebnyi@gmail.com>
2019-05-30 12:28:56 -0400

    Add behave script

---
+++ scripts/behave.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+ROOT_PATH=$(dirname $(dirname $0))
+cd $ROOT_PATH
+
+# ENV=test python manage.py migrate adsrental
+# ENV=test python manage.py migrate
+ENV=test python manage.py loaddata adsrental/fixtures/test.json
+ENV=test python manage.py runserver_plus & > /dev/null
+ENV=test python manage.py behave
