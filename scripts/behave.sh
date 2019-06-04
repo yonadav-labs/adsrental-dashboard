0e09b543b9a4aca9d994e5c0208492c719303ed1
Vlad Emelianov <volshebnyi@gmail.com>
2019-06-03 20:49:54 -0400

    Stop behave on first error

--- scripts/behave.sh
+++ scripts/behave.sh
@@ -6,4 +6,4 @@ cd $ROOT_PATH
 # ENV=test python manage.py migrate
 ENV=test python manage.py loaddata adsrental/fixtures/test.json
 # ENV=test python manage.py runserver_plus & > /dev/null
-ENV=test python manage.py behave --use-existing-database --keepdb $@
+ENV=test python manage.py behave --use-existing-database --keepdb --stop --no-skipped $@
