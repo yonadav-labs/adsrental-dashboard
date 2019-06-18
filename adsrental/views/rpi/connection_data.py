a9e7b0e798c59ec93a663726f9703b308be77cd2
Vlad Emelianov <volshebnyi@gmail.com>
2019-06-18 19:02:04 -0400

    FIx connection data

--- adsrental/views/rpi/connection_data.py
+++ adsrental/views/rpi/connection_data.py
@@ -28,14 +28,14 @@ class ConnectionDataView(View):
                 'result': False,
             })
 
-        active_accounts_count = lead.lead_accounts.filter(status__in=LeadAccount.STATUSES_ACTIVE).count()
-        if not active_accounts_count:
-            return JsonResponse({
-                'error': 'Not available',
-                # 'shutdown': True,
-                'hostname': '',
-                'result': False,
-            })
+        # active_accounts_count = lead.lead_accounts.filter(status__in=LeadAccount.STATUSES_ACTIVE).count()
+        # if not active_accounts_count:
+        #     return JsonResponse({
+        #         'error': 'Not available',
+        #         # 'shutdown': True,
+        #         'hostname': '',
+        #         'result': False,
+        #     })
 
         if not raspberry_pi.is_proxy_tunnel:
             ec2_instance = raspberry_pi.get_ec2_instance()
@@ -54,8 +54,8 @@ class ConnectionDataView(View):
             'rpid': raspberry_pi.rpid,
             'hostname': raspberry_pi.proxy_hostname or '',
             'user': raspberry_pi.TUNNEL_USER,
-            'tunnel_port': raspberry_pi.tunnel_port,
-            'rtunnel_port': raspberry_pi.rtunnel_port,
+            'tunnel_port': raspberry_pi.tunnel_port or '',
+            'rtunnel_port': raspberry_pi.rtunnel_port or '',
             'is_proxy_tunnel': True,
             'is_beta': raspberry_pi.is_proxy_tunnel or raspberry_pi.is_beta,
             'result': True,
