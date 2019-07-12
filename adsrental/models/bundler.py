3c6072309a9c6972ef946424aac6f8daf11f5ec9
Vlad Emelianov <volshebnyi@gmail.com>
2019-07-12 23:43:43 +0300

    Add bundler chargebacks  in last 30 days

--- adsrental/models/bundler.py
+++ adsrental/models/bundler.py
@@ -2,6 +2,8 @@ import decimal
 
 from django.db import models
 
+from adsrental.models.bundler_payment import BundlerPayment
+
 
 class Bundler(models.Model):
     '''
@@ -38,10 +40,20 @@ class Bundler(models.Model):
     facebook_screenshot_third_parent_payment = models.DecimalField(default=decimal.Decimal(0.00), max_digits=8, decimal_places=2, help_text='Amount to withdraw from lead payment to be paid to parent bundler.')
     google_third_parent_payment = models.DecimalField(default=decimal.Decimal(0.00), max_digits=8, decimal_places=2, help_text='Amount to withdraw from lead payment to be paid to third parent bundler.')
     amazon_third_parent_payment = models.DecimalField(default=decimal.Decimal(0.00), max_digits=8, decimal_places=2, help_text='Amount to withdraw from lead payment to be paid to third parent bundler.')
-    facebook_chargeback = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook accounts.')
     facebook_screenshot_chargeback = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook Screenshot accounts.')
+    facebook_chargeback = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook accounts.')
+    facebook_chargeback_30_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook 30+ days active accounts.')
+    facebook_chargeback_60_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook 60+ days active accounts.')
+    facebook_chargeback_90_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Facebook 90+ days active accounts.')
     google_chargeback = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Google accounts.')
+    google_chargeback_30_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Google 30+ days active accounts.')
+    google_chargeback_60_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Google 60+ days active accounts.')
+    google_chargeback_90_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Google 90+ days active accounts.')
     amazon_chargeback = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for AMazon accounts.')
+    amazon_chargeback_30_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Amazon 30+ days active accounts.')
+    amazon_chargeback_60_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Amazon 60+ days active accounts.')
+    amazon_chargeback_90_days = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='Chargeback value for Amazon 90+ days active accounts.')
+    chargeback_streak = models.DecimalField(default=CHARGEBACK_PAYMENT, max_digits=8, decimal_places=2, help_text='How many chargebacks bundler can have in rolling 30 days period.')
     parent_bundler = models.ForeignKey('adsrental.Bundler', null=True, blank=True, on_delete=models.SET_NULL, help_text='Bundler that gets part of the payment')
     second_parent_bundler = models.ForeignKey('adsrental.Bundler', related_name='second_child_bundler', null=True, blank=True, on_delete=models.SET_NULL, help_text='Second Bundler that gets part of the payment')
     third_parent_bundler = models.ForeignKey('adsrental.Bundler', related_name='third_child_bundler', null=True, blank=True, on_delete=models.SET_NULL, help_text='Third Bundler that gets part of the payment')
@@ -57,6 +69,51 @@ class Bundler(models.Model):
     @classmethod
     def get_by_adsdb_id(cls, adsdb_id: str) -> 'Bundler':
         return cls.objects.filter(adsdb_id=adsdb_id).first()
+
+    def is_chargeback_enabled(self, lead_account):
+        now = timezone.localtime(timezone.now())
+        chargeback_count = BundlerPayment.objects.filter(
+            create__gt=now - datetime.timedelta(days=30), bundler=self
+        ).exclude(lead_account=lead_account).count()
+        return chargeback_count <= self.chargeback_streak
+
+    def get_chargeback(self, lead_account) -> decimal.Decimal:
+        if not lead_account.charge_back:
+            return decimal.Decimal('0.00')
+
+        if not self.is_chargeback_enabled(lead_account):
+            return decimal.Decimal('0.00')
+
+        active_days = lead_account.get_active_days()
+        if lead_account.account_type in lead_account.ACCOUNT_TYPES_FACEBOOK:
+            if active_days >= 90 and self.facebook_chargeback_90_day:
+                return self.facebook_chargeback_90_days
+            if active_days >= 60 and self.facebook_chargeback_60_day:
+                return self.facebook_chargeback_60_days
+            if active_days >= 30 and self.facebook_chargeback_30_day:
+                return self.facebook_chargeback_30_days
+            if self.facebook_chargeback:
+                return self.facebook_chargeback
+        if lead_account.account_type == lead_account.ACCOUNT_TYPE_GOOGLE:
+            if active_days >= 90 and self.google_chargeback_90_day:
+                return self.google_chargeback_90_days
+            if active_days >= 60 and self.google_chargeback_60_day:
+                return self.google_chargeback_60_days
+            if active_days >= 30 and self.google_chargeback_30_day:
+                return self.google_chargeback_30_days
+            if self.google_chargeback:
+                return self.google_chargeback
+        if lead_account.account_type == lead_account.ACCOUNT_TYPE_AMAZON:
+            if active_days >= 90 and self.amazon_chargeback_90_day:
+                return self.amazon_chargeback_90_days
+            if active_days >= 60 and self.amazon_chargeback_60_day:
+                return self.amazon_chargeback_60_days
+            if active_days >= 30 and self.amazon_chargeback_30_day:
+                return self.amazon_chargeback_30_days
+            if self.amazon_chargeback:
+                return self.amazon_chargeback
+
+        return decimal.Decimal('0.00')
 
     def __str__(self) -> str:
         return self.name
