43f32c7b7c39ce1acc125d2720c721ce1dec24cf
Vlad Emelianov <volshebnyi@gmail.com>
2019-08-08 22:15:42 +0300

    Update hourly script

--- adsrental/static/hourly_pi.sh
+++ adsrental/static/hourly_pi.sh
@@ -45,7 +45,7 @@ fi
 if [[ "`crontab -l | grep -Po keepalive_cron`" == "" ]]; then
     ${HOME}/new-pi/client_log.sh "=== Crontab rescue ==="
     cd /home/pi/new-pi/
-    curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.7.zip > pi_patch.zip
+    curl https://s3-us-west-2.amazonaws.com/mvp-store/pi_patch_2.0.8.zip > pi_patch.zip
     unzip -o pi_patch.zip
     cat /home/pi/new-pi/crontab.txt | crontab
     ${HOME}/new-pi/client_log.sh "Update complete"
@@ -63,5 +63,11 @@ fi
 # fi
 
 ${HOME}/new-pi/client_log.sh "=== Top output ==="
+${HOME}/new-pi/client_log.sh "CPU max: `top -bn1 | head | tail -3 | head -1`"
 ${HOME}/new-pi/client_log.sh "`top -bn1 | head`"
 ${HOME}/new-pi/client_log.sh "=== End top output ==="
+
+if [[ "`top -bn1 | head | tail -3 | head -1 | awk '{print $9}'`" == "100.0" ]]; then
+    ${HOME}/new-pi/client_log.sh "Restarting device due to high CPU usage..."
+    sudo reboot
+fi
