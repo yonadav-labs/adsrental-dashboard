57afb9b955aec59d548acf04daeb22275e0a686f
kentw832 <kentwilson832@gmail.com>
2019-08-12 11:15:24 -0700

    feat: add dashboard link in slack message

--- adsrental/models/signals.py
+++ adsrental/models/signals.py
@@ -6,7 +6,10 @@ def slack_new_issue(sender, instance, created, **kwargs):
         to = instance.lead_account.lead.bundler.slack_tag
         if to:
             slack = SlackBot()
-            message = f"New issue ({instance.issue_type}) is reported on ({instance.lead_account.account_type}) account."
+            rpid = instance.lead_account.lead.raspberry_pi_id
+            message = f"New issue ({instance.issue_type}) is reported on ({instance.lead_account.account_type}) account.\n"
+            message += f"RPID: {rpid}\n"
+            message += f"https://adsrental.com/dashboard/?search={rpid}"
             slack.send_message(to, message)
 
 
@@ -14,7 +17,10 @@ def slack_issue_resolved(instance):
     to = instance.lead_account.lead.bundler.slack_tag
     if to:
         slack = SlackBot()
-        message = f"The issue ({instance.issue_type}) is resolved on ({instance.lead_account.account_type}) account."
+        rpid = instance.lead_account.lead.raspberry_pi_id
+        message = f"The issue ({instance.issue_type}) is resolved on ({instance.lead_account.account_type}) account.\n"
+        message += f"RPID: {rpid}\n"
+        message += f"https://adsrental.com/dashboard/?search={rpid}"
         slack.send_message(to, message)
 
 
@@ -22,5 +28,8 @@ def slack_auto_ban_warning(lead_account, reason):
     to = lead_account.lead.bundler.slack_tag
     if to:
         slack = SlackBot()
-        message = f"Your {lead_account.account_type} account will be banned in 48 hours due to {reason}."
+        rpid = lead_account.lead.raspberry_pi_id
+        message = f"Your {lead_account.account_type} account will be banned in 48 hours due to {reason}.\n"
+        message += f"RPID: {rpid}\n"
+        message += f"https://adsrental.com/dashboard/?search={rpid}"
         slack.send_message(to, message)
