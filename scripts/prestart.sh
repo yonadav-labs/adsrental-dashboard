ffc9b0e0c2b932fe6ff39198058acf678fa9625b
Vlad Emelianov <volshebnyi@gmail.com>
2019-08-29 06:40:12 +0700

    Add somaxconn

--- scripts/prestart.sh
+++ scripts/prestart.sh
@@ -4,6 +4,10 @@ set -e
 ROOT_PATH="/app/"
 cd $ROOT_PATH
 
+
+echo "/proc/sys/net/core/somaxconn"
+cat /proc/sys/net/core/somaxconn
+
 #waiting for db
 echo "Waiting for DB..."
 wait-for-it --service db:3306
